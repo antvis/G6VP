437ae8a3ff752741bda6a317dceef9eefb6e37d3
pomelo-nwu <pomelo.lcw@antfin.com>
2021-06-01 17:01:12 +0800

    feat: 简易实现数据映射的方案二

--- packages/gi-canvas/src/index.tsx
+++ packages/gi-canvas/src/index.tsx
@@ -1,4 +1,4 @@
-import Graphin, { Utils } from '@antv/graphin';
+import Graphin, { GraphinData } from '@antv/graphin';
 import React from 'react';
 
 export interface Props {
@@ -6,16 +6,107 @@ export interface Props {
    * @description 配置信息
    */
   config: any;
+  services: {
+    /** 获取初始化接口 */
+    getGraphData: () => Promise<any>;
+    /** 根据ID获取节点或边的详情信息 */
+    getGraphDataById?: (id: string) => Promise<any>;
+    /** 获取一度下钻数据 */
+    getExploreGraphByDegree?: (degree: number, id: string) => Promise<any>;
+  };
 }
-const GISDK = props => {
-  const { config } = props;
-  const data = Utils.mock(10)
-    .circle()
-    .graphin();
+
+const getMapping = () => {
+  const Mapping = new Map();
+  return (enumValue, value) => {
+    const current = Mapping.get(enumValue);
+    if (current) {
+      Mapping.set(enumValue, [...current, value]);
+    } else {
+      Mapping.set(enumValue, [value]);
+    }
+    return Mapping;
+  };
+};
+
+/** 数据映射函数  需要根据配置自动生成*/
+const transform = (s, config) => {
+  const { node: NodeConfig, edge: EdgeConfig } = config;
+  /** 解构配置项 */
+  const MathNodeConfig = NodeConfig.find(cfg => cfg.enable);
+  const Size = MathNodeConfig?.size.find(s => s.enable);
+  const Color = MathNodeConfig?.color.find(s => s.enable);
+  const Label = MathNodeConfig?.label;
+  /** 分别生成Size和Color的Mapping */
+  const mappingBySize = getMapping();
+  const mappingByColor = getMapping();
+
+  const nodes = s.nodes.map(node => {
+    const { id, data } = node;
+
+    /** 根据Size字段映射的枚举值 */
+    const enumValueBySize = data[Size?.key || 0];
+    const MappingBySize = mappingBySize(enumValueBySize, node);
+    /** 根据Color字段映射的枚举值 */
+    const enumValueByColor = data[Color?.key || 0];
+    const MappingByColor = mappingByColor(enumValueByColor, node);
+
+    /** 根据数组匹配，未来也是需要用户在属性面板上调整位置 */
+    const colorKeys = MappingByColor.keys();
+    const matchColorIndex = [...colorKeys].findIndex(c => c === enumValueByColor);
+    const sizeKeys = MappingBySize.keys();
+    const matchSizeIndex = [...sizeKeys].findIndex(c => c === enumValueBySize);
+
+    return {
+      id: node.id,
+      data: node.data,
+      style: {
+        keyshape: {
+          stroke: Color?.enum?.[matchColorIndex],
+          fill: Color?.enum?.[matchColorIndex],
+          size: Size?.enum?.[matchSizeIndex],
+        },
+        label: {
+          value: data[Label?.key || 'id'],
+        },
+      },
+    };
+  });
+  const edges = s.edges.map(edge => {
+    return edge;
+  });
+  return {
+    nodes,
+    edges,
+  };
+};
+
+const GISDK = (props: Props) => {
+  const { config, services } = props;
+
+  const [state, setState] = React.useState({
+    data: {} as GraphinData,
+    layout: {
+      type: config.layout.id,
+      ...config.layout.options,
+    },
+  });
+
+  React.useEffect(() => {
+    services.getGraphData().then(res => {
+      setState({
+        ...state,
+        data: transform(res, config),
+      });
+    });
+  }, []);
+
+  const { data, layout } = state;
+
   console.log('config', config);
   return (
     <div>
-      <Graphin data={data}></Graphin>
+      <Graphin data={data} layout={layout}></Graphin>
     </div>
   );
 };
