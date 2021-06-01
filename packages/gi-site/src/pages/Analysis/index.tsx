2e457d5f6f74c0dcad76245705dab61b51bcd2bb
pomelo.lcw <pomelo.lcw@antfin.com>
2021-06-01 17:01:12 +0800

    feat: 简易实现数据映射的方案二

--- packages/gi-site/src/pages/Analysis/index.tsx
+++ packages/gi-site/src/pages/Analysis/index.tsx
@@ -2,8 +2,100 @@ import GIMetaPanel from '@alipay/gi-meta';
 import GISDK from '@alipay/graphinsight';
 import { Layout } from 'antd';
 import React from 'react';
+import { getGraphData } from '../../services/index';
 
-const config = {};
+const config = {
+  components: [
+    {
+      id: 'LEGEND-A',
+      categoryId: 'legend',
+      meta: {},
+      props: {},
+      enable: true,
+    },
+    {
+      id: 'LEGEND-Offical',
+      categoryId: 'legend',
+      meta: {},
+      props: {},
+      enable: false,
+    },
+  ],
+  node: [
+    {
+      id: 'graphin-node',
+
+      categoryId: 'node',
+      enable: true,
+      /** style.keyshape.color */
+      color: [
+        /** 第一种是映射模式 */
+        {
+          mode: 'mapping',
+          key: 'type',
+          enum: ['grey', 'blue', 'green', 'yellow'],
+          enable: true,
+        },
+        /** 第二种是固定模式 */
+        {
+          mode: 'fixed',
+          value: 'red',
+          enable: false,
+        },
+      ],
+      /** style.keyshape.size */
+      size: [
+        /** 第一种是映射模式 */
+        {
+          mode: 'mapping',
+          key: 'type',
+          enum: [50, 20, 30, 40, 10, 60],
+          enable: true,
+        },
+        /** 第二种是固定模式 */
+        {
+          mode: 'fixed',
+          value: 30,
+          enable: false,
+        },
+      ],
+      /** style.label */
+      label: {
+        key: 'name',
+      },
+    },
+  ],
+  edge: [
+    {
+      id: 'graphin-edge',
+      categoryId: 'edge',
+      /** style.keyshape.stroke */
+      color: {
+        key: 'type',
+        enum: ['red', 'blue', 'green', 'yellow'],
+      },
+      /** style.keyshape.size */
+      size: {
+        key: 'weight',
+      },
+      /** style.label */
+      label: {
+        key: 'name',
+      },
+    },
+  ],
+  layout: {
+    categoryId: 'layout',
+    id: 'graphin-force',
+    options: {
+      preset: {
+        type: 'concentric',
+      },
+    },
+  },
+  graph: {},
+};
+export type Config = typeof config;
 
 const Analysis = () => {
   return (
@@ -12,7 +104,7 @@ const Analysis = () => {
       <GIMetaPanel />
       <h1>GraphInsight 核心画布渲染区域</h1>
 
-      <GISDK config={config} />
+      <GISDK config={config} services={{ getGraphData }} />
     </Layout>
   );
 };
