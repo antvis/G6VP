2d26c467c910d39ee0501b11ee708543bc7ecf09
pomelo-nwu <pomelo.lcw@antfin.com>
2021-06-09 23:24:51 +0800

    feat:引入Lockr作为本地数据存储工具，同时为初始化请求服务添加trans过滤函数

--- packages/gi-site/src/services/index.ts
+++ packages/gi-site/src/services/index.ts
@@ -1,48 +1,26 @@
-import liaoyuan from '../mock/liaoyuan.json';
+import Lockr from 'lockr';
 
 export class GraphDataBase {
   source: any;
-  constructor(s: any) {
-    this.source = s;
-  }
+  constructor() {}
   graph() {
     return this.source;
   }
 }
 
-const { nodes, edges } = liaoyuan.result;
-/** 需要标准化 */
-const data = {
-  nodes: nodes.map(n => {
-    return { data: n, id: n.uri }; // 这里需要在数据处理层，让用户自己指定ID
-  }),
-  edges: edges.map(e => {
-    return {
-      data: e,
-      source: e.fromNodeUri, //这里要么要求用户上传标准的JSON，字段必须用target/source，或者就有一个专门的数据处理中心
-      target: e.toNodeUri,
-    };
-  }),
-};
-
 /** 未来 GraphDataBase 可以换成 neo4j 或者 geabase 等数据库服务 */
-const db = new GraphDataBase(data);
+// const db = new GraphDataBase();
 
-export const getGraphData = () => {
+export const getGraphData = transFn => {
   return new Promise(resolve => {
-    const data = db.graph();
+    let data = Lockr.get(Lockr.get('projectId')); // db.graph();
 
     // 这里需要用户从组件市场里定义初始化逻辑
 
-    const nodes = data.nodes.filter(node => {
-      return node.data.type === 'ENTITY' || node.data.type === 'EVENT';
-    });
-    const edges = getEdgesByNodes(nodes, data.edges);
-
-    return resolve({
-      nodes,
-      edges,
-    }); //这里需要一个规范的图结构
+    if (transFn) {
+      data = transFn(data);
+    }
+    return resolve(data); //这里需要一个规范的图结构
   });
 };
 
@@ -60,7 +38,7 @@ export function getEdgesByNodes(nodes, edges) {
 /** 根据节点获取其一度关系 */
 export const getSubGraphData = (ids: string[]) => {
   return new Promise(resolve => {
-    const data = db.graph();
+    const data = Lockr.get(Lockr.get('projectId')); // db.graph();
 
     /** Start：组件市场里定义的逻辑;*/
 
