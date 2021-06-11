e3e1d179b211940f5cf50652e1d0337a5a11a2ba
pomelo.lcw <pomelo.lcw@antfin.com>
2021-06-11 10:03:57 +0800

    feat:串联数据逻辑

--- packages/gi-site/src/pages/Analysis/redux.ts
+++ packages/gi-site/src/pages/Analysis/redux.ts
@@ -1,34 +1,103 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import Lockr from 'lockr';
 import { createStore } from 'redux';
+
 interface Action {
   type: string;
   payload: any;
 }
 
+const project = Lockr.get('project');
+
 const initialState = {
-  data: { nodes: [], edges: [] },
-  layout: {
-    type: 'concentric',
-    options: {},
+  config: {
+    components: [
+      {
+        id: 'Legend',
+        meta: {},
+        props: {},
+        enable: true,
+      },
+    ],
+    node: [
+      {
+        id: 'graphin-node',
+        enable: true,
+        /** style.keyshape.color */
+        color: [
+          /** 第一种是映射模式 */
+          {
+            mode: 'mapping',
+            key: 'type',
+            enum: ['grey', 'blue', 'green', 'yellow', 'pink'],
+            enable: true,
+          },
+          /** 第二种是固定模式 */
+          {
+            mode: 'fixed',
+            value: 'red',
+            enable: false,
+          },
+        ],
+        /** style.keyshape.size */
+        size: [
+          /** 第一种是映射模式 */
+          {
+            mode: 'mapping',
+            key: 'type',
+            enum: [40, 20, 30, 20, 10],
+            enable: true,
+          },
+          /** 第二种是固定模式 */
+          {
+            mode: 'fixed',
+            value: 30,
+            enable: false,
+          },
+        ],
+        /** style.label */
+        label: {
+          key: 'id',
+        },
+      },
+    ],
+    edge: [
+      {
+        id: 'graphin-edge',
+        /** style.keyshape.stroke */
+        color: {
+          key: 'type',
+          enum: ['red', 'blue', 'green', 'yellow'],
+        },
+        /** style.keyshape.size */
+        size: {
+          key: 'weight',
+        },
+        /** style.label */
+        label: {
+          key: 'id',
+        },
+      },
+    ],
+    layout: {
+      id: 'graphin-force',
+      options: {
+        animation: false,
+        preset: {
+          type: 'concentric',
+        },
+      },
+    },
   },
-  tooltip: {
-    placement: 'top',
-    visible: true,
-    hasArrow: true,
-    keys: ['id'],
-  },
-  allKeys: ['id', 'type'],
 };
 
 const RootReducers = (state = initialState, action: Action) => {
   const { type, ...payload } = action;
   switch (type) {
-    case 'Update_Data':
-      const allKeys = Object.keys(payload.data.nodes[0]);
+    case 'Update:Config':
       return {
         ...state,
         ...payload,
-        allKeys,
       };
     case 'Update_Layout':
       return {
