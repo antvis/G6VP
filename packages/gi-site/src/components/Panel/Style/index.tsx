c31561ad0e1540a146adb6f1c748528340d4f61b
pomelo-nwu <pomelo.lcw@antfin.com>
2021-06-11 10:03:57 +0800

    feat:串联数据逻辑

--- packages/gi-site/src/components/Panel/Style/index.tsx
+++ packages/gi-site/src/components/Panel/Style/index.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from 'antd';
 import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './index.less';
 
 const { TabPane } = Tabs;
@@ -7,11 +8,39 @@ const callback = () => {};
 
 export interface StylePanelProps {}
 const StylePanel: React.FunctionComponent<StylePanelProps> = props => {
+  const dispatch = useDispatch();
+  const config = useSelector(state => state.config);
+
+  const handleClick = () => {
+    dispatch({
+      type: 'Update:Config',
+      config: {
+        ...config,
+        node: [
+          {
+            id: 'graphin-node',
+            enable: true,
+            /** style.keyshape.color */
+            color: [
+              /** 第一种是映射模式 */
+              {
+                mode: 'mapping',
+                key: 'entityTypeName',
+                enum: ['grey', 'blue', 'green', 'yellow', 'pink'],
+                enable: true,
+              },
+            ],
+          },
+        ],
+      },
+    });
+  };
+
   return (
     <div>
       <Tabs defaultActiveKey="node" onChange={callback}>
         <TabPane tab="节点样式" key="node">
-          Node
+          <button onClick={handleClick}> Node</button>
         </TabPane>
         <TabPane tab="边的样式" key="edge">
           Edge
--- packages/gi-site/src/pages/Analysis/index.tsx
+++ packages/gi-site/src/pages/Analysis/index.tsx
@@ -1,237 +1,13 @@
 import GISDK, { GIContext } from '@alipay/graphinsight';
-import { Input } from 'antd';
 import Lockr from 'lockr';
 import React from 'react';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import { ConfigationPanel, Navbar, Sidebar } from '../../components';
 import { getEdgesByNodes } from '../../services';
 import { getGraphData, getSubGraphData } from '../../services/index';
 import { configSchema, navbarOptions } from './Constants';
 import './index.less';
 import store from './redux';
-
-const { TextArea } = Input;
-
-const configs = {
-  liaoyuan: {
-    components: [
-      {
-        id: 'Legend',
-        categoryId: 'legend',
-        meta: {},
-        props: {},
-        enable: true,
-      },
-      {
-        id: 'NodeTooltip',
-        categoryId: 'tooltip',
-        meta: {},
-        props: {
-          mappingKeys: ['name', 'type'],
-        },
-        enable: false,
-      },
-      {
-        id: 'MiniMap',
-        meta: {},
-        props: {},
-        enable: false,
-      },
-      {
-        id: 'Liaoyuan-Click-Entity-Node',
-        categoryId: 'interaction',
-        meta: {},
-        props: {},
-        enable: true,
-      },
-      {
-        id: 'Liaoyuan-Click-Event-Node',
-        categoryId: 'interaction',
-        meta: {},
-        props: {},
-        enable: true,
-      },
-    ],
-    node: [
-      {
-        id: 'graphin-node',
-
-        categoryId: 'node',
-        enable: true,
-        /** style.keyshape.color */
-        color: [
-          /** 第一种是映射模式 */
-          {
-            mode: 'mapping',
-            key: 'type',
-            enum: ['grey', 'blue', 'green', 'yellow', 'pink'],
-            enable: true,
-          },
-          /** 第二种是固定模式 */
-          {
-            mode: 'fixed',
-            value: 'red',
-            enable: false,
-          },
-        ],
-        /** style.keyshape.size */
-        size: [
-          /** 第一种是映射模式 */
-          {
-            mode: 'mapping',
-            key: 'type',
-            enum: [40, 20, 30, 20, 10],
-            enable: true,
-          },
-          /** 第二种是固定模式 */
-          {
-            mode: 'fixed',
-            value: 30,
-            enable: false,
-          },
-        ],
-        /** style.label */
-        label: {
-          key: 'name',
-        },
-      },
-    ],
-    edge: [
-      {
-        id: 'graphin-edge',
-        categoryId: 'edge',
-        /** style.keyshape.stroke */
-        color: {
-          key: 'type',
-          enum: ['red', 'blue', 'green', 'yellow'],
-        },
-        /** style.keyshape.size */
-        size: {
-          key: 'weight',
-        },
-        /** style.label */
-        label: {
-          key: 'name',
-        },
-      },
-    ],
-    layout: {
-      categoryId: 'layout',
-      id: 'dagre', //'graphin-force',
-      options: {
-        rankdir: 'LR', // 可选，默认为图的中心
-        align: undefined, // 可选
-        nodesep: 15, // 可选
-        ranksep: 40, // 可选
-        // preset: {
-        //   type: 'concentric',
-        // },
-      },
-    },
-    graph: {},
-  },
-  demo: {
-    components: [
-      {
-        id: 'Legend',
-        categoryId: 'legend',
-        meta: {},
-        props: {},
-        enable: true,
-      },
-    ],
-    node: [
-      {
-        id: 'graphin-node',
-        categoryId: 'node',
-        enable: true,
-        /** style.keyshape.color */
-        color: [
-          /** 第一种是映射模式 */
-          {
-            mode: 'mapping',
-            key: 'type',
-            enum: ['grey', 'blue', 'green', 'yellow', 'pink'],
-            enable: true,
-          },
-          /** 第二种是固定模式 */
-          {
-            mode: 'fixed',
-            value: 'red',
-            enable: false,
-          },
-        ],
-        /** style.keyshape.size */
-        size: [
-          /** 第一种是映射模式 */
-          {
-            mode: 'mapping',
-            key: 'type',
-            enum: [40, 20, 30, 20, 10],
-            enable: true,
-          },
-          /** 第二种是固定模式 */
-          {
-            mode: 'fixed',
-            value: 30,
-            enable: false,
-          },
-        ],
-        /** style.label */
-        label: {
-          key: 'id',
-        },
-      },
-    ],
-    layout: {
-      categoryId: 'layout',
-      id: 'dagre', //'graphin-force',
-      options: {},
-    },
-  },
-  wangshang: {
-    components: [
-      {
-        id: 'Legend',
-        categoryId: 'legend',
-        meta: {},
-        props: {},
-        enable: true,
-      },
-    ],
-    node: [
-      {
-        id: 'graphin-node',
-        categoryId: 'node',
-        enable: true,
-        color: [
-          {
-            mode: 'mapping',
-            key: 'entityTypeName',
-            enum: ['grey', 'blue', 'green', 'yellow', 'pink'],
-            enable: true,
-          },
-        ],
-        size: [
-          {
-            mode: 'fixed',
-            value: 30,
-            enable: true,
-          },
-        ],
-      },
-    ],
-    layout: {
-      categoryId: 'layout',
-      id: 'graphin-force',
-      options: {
-        preset: {
-          type: 'concentric',
-        },
-      },
-    },
-  },
-};
 
 const services = {
   liaoyuan: {
@@ -256,7 +32,6 @@ const services = {
     getGraphData: getGraphData,
   },
 };
-export type Config = typeof config;
 
 const TestComponents = () => {
   const gi = React.useContext(GIContext);
@@ -266,8 +41,15 @@ const TestComponents = () => {
 const Analysis = props => {
   const { history, match } = props;
   const { projectId } = match.params;
+
+  const config = useSelector(state => state.config);
+
+  const dispatch = useDispatch();
+
   console.log('props', props, projectId);
+
   Lockr.set('projectId', projectId);
+
   const [state, setState] = React.useState({
     activeNavbar: 'style',
     collapse: false,
@@ -281,33 +63,49 @@ const Analysis = props => {
       collapse: isSame ? !state.collapse : false,
     });
   };
+  React.useEffect(() => {
+    const project = Lockr.get('project');
+    const { config } = project.find(p => {
+      return p.id === projectId;
+    });
+    dispatch({
+      type: 'Update:Config',
+      config,
+    });
+  }, [projectId]);
   const service = services[projectId];
-  const config = configs[projectId];
+  // const config = configs[projectId];
 
   return (
-    <Provider store={store}>
-      <div className="gi">
-        <div className="gi-navbar">
-          <Navbar history={history} />
+    <div className="gi">
+      <div className="gi-navbar">
+        <Navbar history={history} />
+      </div>
+      <div className="gi-analysis">
+        <div className="gi-analysis-sidebar">
+          <Sidebar options={navbarOptions} value={state.activeNavbar} onChange={handleChangeNavbar} />
         </div>
-        <div className="gi-analysis">
-          <div className="gi-analysis-sidebar">
-            <Sidebar options={navbarOptions} value={state.activeNavbar} onChange={handleChangeNavbar} />
-          </div>
-          <div className={`gi-analysis-conf ${state.collapse ? 'collapse' : ''}`}>
-            <ConfigationPanel value={state.activeNavbar} options={configSchema} />
-          </div>
-          <div className="gi-analysis-workspace">
-            <div className="gi-analysis-canvas">
-              <GISDK config={config} services={service}>
-                <TestComponents />
-              </GISDK>
-            </div>
+        <div className={`gi-analysis-conf ${state.collapse ? 'collapse' : ''}`}>
+          <ConfigationPanel value={state.activeNavbar} options={configSchema} />
+        </div>
+        <div className="gi-analysis-workspace">
+          <div className="gi-analysis-canvas">
+            <GISDK config={config} services={service}>
+              <TestComponents />
+            </GISDK>
           </div>
         </div>
       </div>
+    </div>
+  );
+};
+
+const WrapAnalysis = props => {
+  return (
+    <Provider store={store}>
+      <Analysis {...props} />
     </Provider>
   );
 };
 
-export default Analysis;
+export default WrapAnalysis;
