dd873d13e5c735100ed60d209e75fef8debc9409
糖莹 <xuying.xu@mybank.cn>
2021-06-13 10:35:26 +0800

    PullRequest: 6 develop.xy
    Merge branch develop.xy of git@gitlab.alipay-inc.com:graphinsight/graphInsight.git into master
    https://code.alipay.com/graphinsight/graphInsight/pull_requests/6
    
    Signed-off-by: 山果 <pomelo.lcw@antgroup.com>
    
    
    * 创建页面
    
    * 创建项目初步完成
    
    * merge
    
    * 构建流程调整

---
+++ packages/gi-site/src/pages/Workspace/defaultData.ts
@@ -0,0 +1,9 @@
+import demo from '../../mock/demo.json';
+import enterprise from '../../mock/enterprise.json';
+import liaoyuan from '../../mock/liaoyuan.json';
+
+export const defaultData = {
+  GIConfig: demo,
+  knowledgeGraph: enterprise,
+  riskControl: liaoyuan,
+};
