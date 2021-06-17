6e21a6286468ea036e7690679ae1ce760153b57b
wb-zx669722 <wb-zx669722@antgroup.com>
2021-06-17 17:35:34 +0800

    feat: 官网初构

---
+++ packages/gi-site/src/pages/Home/constant.ts
@@ -0,0 +1,106 @@
+export const levelTitleMapping = {
+  product: {
+    title: '产品特性',
+    description: '借助强大的数据可视化功能，您可以发现揭示威胁，风险和机遇的见解，从而从中获得启发',
+    list: [
+      {
+        title: '视觉映射',
+        desc: '简单易用的操作，让你的数据栩栩如生',
+        url:
+          'https://gw.alipayobjects.com/os/sa/cms/images/k7k4tcke/73dbe4db-56df-4742-9ec9-b4485c085afb_w876_h536.png',
+      },
+      {
+        title: '自动布局',
+        desc: '为你的关系数据选择一个合适的布局方案',
+        url:
+          'https://gw.alipayobjects.com/os/sa/cms/images/k7k4tcke/73dbe4db-56df-4742-9ec9-b4485c085afb_w876_h536.png',
+      },
+      {
+        title: '组件市场',
+        desc: '丰富的组件选择，可以在线定制，并且及时消费',
+        url:
+          'https://gw.alipayobjects.com/os/sa/cms/images/k7k4tcke/73dbe4db-56df-4742-9ec9-b4485c085afb_w876_h536.png',
+      },
+      {
+        title: '开放定制',
+        desc: '零成本，快速集成到你的业务系统中',
+        url:
+          'https://gw.alipayobjects.com/os/sa/cms/images/k7k4tcke/73dbe4db-56df-4742-9ec9-b4485c085afb_w876_h536.png',
+      },
+    ],
+  },
+  resolve: {
+    title: '解决方案',
+    description: '构建领域知识图谱，打造行业解决方案，释放数据互联的力量，助力企业洞悉关联数据的商业价值',
+    list: [
+      {
+        title: '图数据库',
+        desc:
+          '基于图模型，使用图结构进行语义操作的数据库，使用顶点、边和属性来表示和存储数据，支持数据的增删改查操作。',
+        url:
+          'https://gw.alipayobjects.com/os/sa/cms/images/k7k4tcke/73dbe4db-56df-4742-9ec9-b4485c085afb_w876_h536.png',
+        btnText: '立即使用',
+      },
+      {
+        title: '云安全',
+        desc:
+          '将多源异构的安全数据关联，并将可视化技术有效应用于安全分析场景，提升安全分析人员感知、分析和理解网络安全问题的能力。',
+        url:
+          'https://gw.alipayobjects.com/os/sa/cms/images/k7k4tcke/73dbe4db-56df-4742-9ec9-b4485c085afb_w876_h536.png',
+        btnText: '立即使用',
+      },
+      {
+        title: '企业风控',
+        desc:
+          '结合各个细分领域、业务场景的企业数据可视化，可以更清晰发现和识别风险，从而将数据变成实际可用的风控产品和服务。',
+        url:
+          'https://gw.alipayobjects.com/os/sa/cms/images/k7k4tcke/73dbe4db-56df-4742-9ec9-b4485c085afb_w876_h536.png',
+        btnText: '立即使用',
+      },
+      {
+        title: '知识图谱',
+        desc:
+          '基于图模型，使用图结构进行语义操作的数据库，使用顶点、边和属性来表示和存储数据，支持数据的增删改查操作。',
+        url:
+          'https://gw.alipayobjects.com/os/sa/cms/images/k7k4tcke/73dbe4db-56df-4742-9ec9-b4485c085afb_w876_h536.png',
+        btnText: '在线使用',
+      },
+      {
+        title: '金融管理',
+        desc:
+          '这里是对解决方案的能力描述。这里是对解决方案的能力描述。这里是对解决方案的能力描述。这里是对解决方案的能力描述。',
+        url:
+          'https://gw.alipayobjects.com/os/sa/cms/images/k7k4tcke/73dbe4db-56df-4742-9ec9-b4485c085afb_w876_h536.png',
+        btnText: '立即使用',
+      },
+      {
+        title: '金融风控',
+        desc:
+          '这里是对解决方案的能力描述。这里是对解决方案的能力描述。这里是对解决方案的能力描述。这里是对解决方案的能力描述。',
+        url:
+          'https://gw.alipayobjects.com/os/sa/cms/images/k7k4tcke/73dbe4db-56df-4742-9ec9-b4485c085afb_w876_h536.png',
+        btnText: '立即使用',
+      },
+    ],
+  },
+  client: {
+    title: '客户案例',
+    description: '与他们在GraphInsight上共同完成数字化转型',
+    list: [
+      {
+        title: '燎原计划',
+        desc:
+          '这里是案例描述这里是案例描述这里是案例描述这里是案例描述这里是案例描述这里是案例描述这里是案例描述这里是案例描述。',
+        url:
+          'https://gw.alipayobjects.com/os/sa/cms/images/k7k4tcke/73dbe4db-56df-4742-9ec9-b4485c085afb_w876_h536.png',
+      },
+      {
+        title: '行业图谱',
+        desc:
+          '这里是案例描述这里是案例描述这里是案例描述这里是案例描述这里是案例描述这里是案例描述这里是案例描述这里是案例描述。',
+        url:
+          'https://gw.alipayobjects.com/os/sa/cms/images/k7k4tcke/73dbe4db-56df-4742-9ec9-b4485c085afb_w876_h536.png',
+      },
+    ],
+  },
+};
