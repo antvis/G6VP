63c67a2d8a7fdce21ba69a5bff3f1a7fe8eae01c
pomelo-nwu <pomelo.lcw@antfin.com>
2021-06-21 15:15:48 +0800

    feat:临时方案：用户自定义meta，需要从组件市场中获取

--- packages/gi/src/components/Legend/meta.tsx
+++ packages/gi/src/components/Legend/meta.tsx
@@ -1,7 +1,19 @@
-export default [
-  {
-    id: 'sortkey',
-    default: 'type',
-    type: 'select',
-  }
-]
+const getMeta = context => {
+  const { data, allKeys = ['id', 'type'] } = context;
+  const options = allKeys.map(c => {
+    return {
+      value: c,
+      label: c,
+    };
+  });
+  return {
+    sortkey: {
+      name: '映射字段',
+      type: 'select',
+      default: 'type',
+      options,
+    },
+  };
+};
+
+export default getMeta;
