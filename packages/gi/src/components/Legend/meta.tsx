8bbc3bfa54a3be5d224955066cfe8dbc2fbb9f4b
pomelo.lcw <pomelo.lcw@antfin.com>
2021-06-22 16:55:46 +0800

    feat:添加组件meta信息的categoryId字段，用于UI展示

--- packages/gi/src/components/Legend/meta.tsx
+++ packages/gi/src/components/Legend/meta.tsx
@@ -7,9 +7,13 @@ const getMeta = context => {
     };
   });
   return {
-    type: 'group',
+    /** 分类信息 */
+    categoryId: 'components-analysis',
+    id: 'NodeLegend',
+
+    type: 'group', //这个可以不写
+    fold: false, // 这个可以不写
     name: '节点图例',
-    fold: false,
     children: {
       sortkey: {
         name: '映射字段',
