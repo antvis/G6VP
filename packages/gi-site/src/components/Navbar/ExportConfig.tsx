eb7983f4faebdcb5f2848dcd91dc55f787061240
wangbo <wb-wb547578@antfin.com>
2021-06-22 17:30:46 +0800

    feat: 导出模块调整及下载复制功能开发

--- packages/gi-site/src/components/Navbar/ExportConfig.tsx
+++ packages/gi-site/src/components/Navbar/ExportConfig.tsx
@@ -1,26 +1,59 @@
 import { useSelector } from 'react-redux';
+import { CopyOutlined, UploadOutlined } from '@ant-design/icons';
+import { Button, message } from 'antd';
+import MonacoEditor from 'react-monaco-editor';
+import dayjs from 'dayjs';
+
+import { copyText, saveAs, generatorconfigToCode } from './utils';
+
+import './index.less';
 
 const ExportConfig = props => {
+  console.log(123);
   const config = useSelector(state => state.config);
-  const exampleCode = `
-    import React, { useState } from "react";
-
-    const config = ${JSON.stringify(config)}
-    const Example = (props) => {
-
-    return (
-        <GISDK
-            key={key}
-            config={config}
-            services={{
-            getGraphData,
-            getSubGraphData,
-            }}
-        >
-        );
+  const exampleCode = generatorconfigToCode(config);
+  /** 复制 */
+  const handleCopy = () => {
+    const flag = copyText(exampleCode);
+    if (flag) {
+      message.success('复制成功');
+    } else {
+      message.error('复制失败');
     }
-    `;
-  return <span>{exampleCode}</span>;
+  };
+
+  /** 下载 */
+  const handleExport = () => {
+    let [code, ext] = [exampleCode, '.js'];
+    saveAs(code, `graphInsight-${dayjs().format('YYYYMMDD HH:mm:ss')}${ext}`);
+  };
+
+  return (
+    <div className="export-panel">
+      <div className="export-panel-editor">
+        <div style={{ width: 770, height: '100%' }}>
+          <MonacoEditor
+            language="javascript"
+            theme="vs-dark"
+            options={{
+              minimap: { enabled: false },
+              readOnly: true
+            }}
+            value={exampleCode}
+          />
+        </div>
+      </div>
+      <div className="export-panel-footer">
+        <Button type="primary" onClick={handleCopy}>
+          <CopyOutlined /> 拷贝
+        </Button>
+        <Button type="primary" onClick={handleExport}>
+          <UploadOutlined /> 导出
+      </Button>
+      </div>
+      {/* <span>{exampleCode}</span> */}
+    </div>
+  );
 };
 
 export default ExportConfig;
