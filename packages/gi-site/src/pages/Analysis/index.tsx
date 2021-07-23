ecf2d785947bc8a312c5788f5b194a56075ea268
pomelo-nwu <pomelo.lcw@antfin.com>
2021-07-23 10:40:09 +0800

    chore:新增组件自选

--- packages/gi-site/src/pages/Analysis/index.tsx
+++ packages/gi-site/src/pages/Analysis/index.tsx
@@ -1,5 +1,4 @@
-import GISDK from '@alipay/graphinsight';
-import localforage from 'localforage';
+import GISDK, { GIComponents } from '@alipay/graphinsight';
 import React from 'react';
 import { Provider, useDispatch, useSelector } from 'react-redux';
 import { Prompt } from 'react-router-dom';
@@ -8,8 +7,10 @@ import Loading from '../../components/Loading';
 import { getProjectById } from '../../services/analysis';
 import { navbarOptions } from './Constants';
 import './index.less';
-import store from './redux';
+import store, { StateType } from './redux';
 import { isObjectEmpty } from './utils';
+/** https://github.com/systemjs/systemjs/blob/main/docs/nodejs.md */
+// const { System } = require('systemjs');
 
 const getServices = id => {
   return [
@@ -25,18 +26,13 @@ const getServices = id => {
     },
   ];
 };
-
-const getSourceData = async id => {
-  const project = await localforage.getItem(id);
-  return project.data;
+const getComponents = id => {
+  return GIComponents(id);
 };
 
 function looseJsonParse(obj) {
   return Function('"use strict";return (' + obj + ')')();
 }
-
-// const services = getServices(projectId);
-// let data = sourceData || getSourceData(projectId);
 
 const genServices = (services, sourceData) => {
   let data = sourceData;
@@ -73,10 +69,11 @@ const genServices = (services, sourceData) => {
 const Analysis = props => {
   const { history, match } = props;
   const { projectId } = match.params;
-  const st = useSelector(state => state);
-  const { config, key, isReady, isSave, activeNavbar, collapse, data, services } = st;
+  const state = useSelector((state: StateType) => state);
+  const { config, key, isReady, isSave, activeNavbar, collapse, data, services, components, refreshComponentKey } =
+    state;
 
-  console.log('Analysis', st);
+  console.log('Analysis', state);
 
   const dispatch = useDispatch();
 
@@ -94,9 +91,17 @@ const Analysis = props => {
       type: 'update:config',
       isReady: false,
     });
+
+    // System.import('https://gw.alipayobjects.com/os/lib/antd/4.16.8/dist/antd.min.js').then(res => {
+    //   console.log('res', res);
+    // });
+
     getProjectById(projectId).then(res => {
       const { config, data } = res as any;
+      /** 目前先Mock，都需要直接从服务端获取services和components，即数据服务和组件市场 */
       const sers = getServices(projectId);
+      const components = getComponents(projectId);
+      /** 动态生成自己的服务实例 */
       const services = genServices(sers, data);
       dispatch({
         type: 'update:config',
@@ -106,6 +111,7 @@ const Analysis = props => {
         services,
         isReady: true,
         activeNavbar: 'style',
+        components,
       });
     });
   }, [projectId]);
@@ -116,6 +122,7 @@ const Analysis = props => {
       ev.returnValue = '配置未保存，确定离开吗？';
     });
   }, []);
+
   const isLoading = isObjectEmpty(config) || !isReady;
   if (isLoading) {
     return (
@@ -136,7 +143,15 @@ const Analysis = props => {
           <Sidebar options={navbarOptions} value={activeNavbar} onChange={handleChangeNavbar} />
         </div>
         <div className={`gi-analysis-conf ${collapse ? 'collapse' : ''}`}>
-          <ConfigationPanel config={config} value={activeNavbar} data={data} services={services} />
+          <ConfigationPanel
+            config={config}
+            value={activeNavbar}
+            data={data}
+            services={services}
+            dispatch={dispatch}
+            components={components}
+            refreshKey={refreshComponentKey}
+          />
         </div>
         <div className="gi-analysis-workspace">
           <div className="gi-analysis-canvas">
--- packages/gi-site/src/pages/Analysis/redux.ts
+++ packages/gi-site/src/pages/Analysis/redux.ts
@@ -1,24 +1,60 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { GIComponentConfig, GIConfig, GIService } from '@alipay/graphinsight';
 import { createStore } from 'redux';
-
 interface Action {
   type: string;
   payload: any;
 }
 
 const initialState = {
+  /** 项目ID */
   id: '',
+  /** 强制渲染的React Key */
   key: Math.random(),
+  /** 画布渲染的配置 */
   config: {},
+  /** 是否准备完毕 */
   isReady: false,
+  /** 是否保存 */
   isSave: true,
+  /** 当前 Sidebar 的值 */
   activeNavbar: '',
+  /** 当前 Sidebar 是否可折叠 */
   collapse: false,
+  /** 所有的数据服务列表 */
   services: [],
+  /** 组件市场的所有组件 */
+  components: [],
+  data: {},
+  refreshComponentKey: Math.random(),
 };
 
-export type StateType = typeof initialState;
-const RootReducers = (state = initialState, action: Action) => {
+export interface StateType {
+  /** 项目ID */
+  id: string;
+  /** 强制渲染的React Key */
+  key: number;
+  /** 强制刷新Components 的Key */
+  refreshComponentKey: number;
+  /** 原始数据 */
+  data: any;
+  /** 画布渲染的配置 */
+  config: GIConfig;
+  /** 是否准备完毕 */
+  isReady: boolean;
+  /** 是否保存 */
+  isSave: boolean;
+  /** 当前 Sidebar 的值 */
+  activeNavbar: string;
+  /** 当前 Sidebar 是否可折叠 */
+  collapse: boolean;
+  /** 所有的数据服务列表 */
+  services: GIService[];
+  /** 组件市场的所有组件 */
+  components: GIComponentConfig[];
+}
+
+const RootReducers = (state: StateType = initialState, action: Action): StateType => {
   const { type, ...payload } = action;
   switch (type) {
     case 'update:config':
@@ -31,7 +67,7 @@ const RootReducers = (state = initialState, action: Action) => {
         ...state,
         ...payload,
       };
-    case 'update:key':
+    case 'update:components':
       return {
         ...state,
         ...payload,
