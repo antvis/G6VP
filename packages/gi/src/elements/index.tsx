d3742c45f2694c122e83da8f10e5ac970289226e
pomelo.lcw <pomelo.lcw@antfin.com>
2021-07-29 18:48:12 +0800

    feat:补全元素物料的信息

--- packages/gi/src/elements/index.tsx
+++ packages/gi/src/elements/index.tsx
@@ -4,5 +4,7 @@ import GraphinNode from './GraphinNode';
 import RectNode from './RectNode';
 import SimpleNode from './SimpleNode';
 
+// TODO fs文件自动写入
+
 /** 图元素市场 */
 export { GraphinNode, RectNode, GraphinEdge, SimpleNode, DonutNode };
