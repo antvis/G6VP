becebf38176a0497aa900d4315d8d696c36957c5
pomelo-nwu <pomelo.lcw@antfin.com>
2021-08-03 10:16:29 +0800

    refactor:remove gi-meta

--- packages/gi-meta/.fatherrc.js
+++
@@ -1,9 +0,0 @@
-export default {
-  // esm: { type: 'rollup', file: 'index', importLibToEs: true },
-  // lessInRollupMode: {},
-  // extractCSS: true,
-  entry: './src/index.ts',
-  esm: 'babel',
-  cjs: 'babel',
-  lessInBabelMode: true,
-};
