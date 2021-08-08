657a83a3d0bba0d09795c61a0a21dd95c4ac2ab7
pomelo.lcw <pomelo.lcw@antfin.com>
2021-08-08 22:22:43 +0800

    feat:重构数据服务模块

---
+++ packages/gi-site/src/components/DataSource/Detail.tsx
@@ -0,0 +1,109 @@
+import { Card, Input, Radio } from 'antd';
+import React from 'react';
+import MonacoEditor from 'react-monaco-editor';
+import './index.less';
+export interface Option {
+  id: string;
+  mode: string;
+  name: string;
+  content: string;
+}
+interface SidebarProps {
+  id: string;
+  mode: string;
+  name: string;
+  content: string;
+  handleSave: (options: Option) => void;
+}
+
+let monacoRef;
+let serviceIDRef;
+let serviceNameRef;
+let serviceModeRef;
+
+const serviceTypes = [
+  {
+    label: 'API',
+    value: 'api',
+    disabled: true,
+  },
+  {
+    label: 'Mock',
+    value: 'mock',
+  },
+];
+
+const Detail: React.FunctionComponent<SidebarProps> = props => {
+  const editorDidMount = editor => {
+    editor.focus();
+  };
+
+  const handleSave = () => {
+    const id = serviceIDRef.state.value;
+    const name = serviceNameRef.state.value;
+    const mode = 'mock'; //serviceModeRef.target.value;
+    const monacoModel = monacoRef.editor.getModel();
+    const content = monacoModel.getValue();
+    console.log(id, name, content, mode);
+    props.handleSave({
+      id,
+      name,
+      content,
+      mode,
+    });
+  };
+
+  const { id, mode, content, name } = props;
+
+  const extra = <div onClick={handleSave}> save | delete </div>;
+
+  return (
+    <div className="gi-services-editor">
+      <Card title="服务详情" extra={extra}>
+        服务ID:
+        <Input
+          placeholder="Basic usage"
+          defaultValue={id}
+          ref={node => {
+            serviceIDRef = node;
+          }}
+        />
+        <br />
+        服务名称:
+        <Input
+          placeholder="Basic usage"
+          defaultValue={name}
+          ref={node => {
+            serviceNameRef = node;
+          }}
+        />
+        服务类型:
+        <Radio.Group
+          options={serviceTypes}
+          onChange={value => {
+            serviceModeRef = value;
+          }}
+          defaultValue={mode}
+        />
+        <br />
+        服务实现：
+        {mode === 'mock' && (
+          <MonacoEditor
+            ref={node => {
+              monacoRef = node;
+            }}
+            width="500px"
+            height="500px"
+            language="json"
+            theme="vs-dark"
+            defaultValue={content}
+            options={{}}
+            editorDidMount={editorDidMount}
+          />
+        )}
+      </Card>
+    </div>
+  );
+};
+
+export default Detail;
