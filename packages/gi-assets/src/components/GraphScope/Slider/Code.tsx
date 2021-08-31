6e7a89d9bf49a2b79c4d02447a67badc9a0ac6c8
pomelo-nwu <pomelo.lcw@antfin.com>
2021-08-31 17:08:21 +0800

    feat:完善graphscope

--- packages/gi-assets/src/components/GraphScope/Slider/Code.tsx
+++ packages/gi-assets/src/components/GraphScope/Slider/Code.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './style.less';
 
-export default () => {
+export default ({ codeList = [] }: { codeList: string[] }) => {
   return (
     <ul className="graphinSight_sider_code_list">
-      <li>console.log(123)</li>
-      <li>console.log(123)</li>
-      <li>console.log(123)</li>
+      {codeList.map(item => (
+        <li key={item}>{item}</li>
+      ))}
     </ul>
   );
 };
