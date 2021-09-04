f6859f1d6dbbb8f147c4fd14e79341c1705b5d5c
pomelo-nwu <pomelo.lcw@antfin.com>
2021-09-04 08:18:55 +0800

    feat:新增切换主题

--- packages/gi-site/src/components/Navbar/index.tsx
+++ packages/gi-site/src/components/Navbar/index.tsx
@@ -102,10 +102,14 @@ const Navbar = ({ projectId }: NavbarProps) => {
   const menu = (
     <>
       <Tooltip title="保存">
-        <Button icon={<SaveOutlined />} onClick={handleSave}>保存</Button>
+        <Button icon={<SaveOutlined />} onClick={handleSave}>
+          保存
+        </Button>
       </Tooltip>
       <Tooltip title="导出">
-        <Button icon={<ExportOutlined />} onClick={handleOutOpen}>导出</Button>
+        <Button icon={<ExportOutlined />} onClick={handleOutOpen}>
+          导出
+        </Button>
       </Tooltip>
       <Button onClick={backWorkspace}>返回列表</Button>
     </>
---
+++ packages/gi-site/src/components/ThemeVars/site.ts
@@ -0,0 +1,16 @@
+export default {
+  light: {
+    '--primary-color': '#363b40',
+    '--component-background': '#fff',
+    '--hr-color': '#ddd',
+    // 主要文本颜色
+    '--primary-text-color': 'rgba(0, 0, 0, 0.85)',
+  },
+  dark: {
+    '--primary-color': '#363b40',
+    '--component-background': '#171819',
+    '--hr-color': '#303030',
+    // 主要文本颜色
+    '--primary-text-color': 'rgba(255, 255, 255, 0.85)',
+  },
+};
