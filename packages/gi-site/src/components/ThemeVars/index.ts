baea341d1459f2e881da2f2a04aa3ee7d01ec384
pomelo-nwu <pomelo.lcw@antfin.com>
2021-09-06 16:01:46 +0800

    feat:主题设置

--- packages/gi-site/src/components/ThemeVars/index.ts
+++ packages/gi-site/src/components/ThemeVars/index.ts
@@ -1,10 +1,13 @@
 import dataVTheme from './datav';
+import siteTheme from './site';
 const theme = {
   light: {
     ...dataVTheme.light,
+    ...siteTheme.light,
   },
   dark: {
     ...dataVTheme.dark,
+    ...siteTheme.dark,
   },
 };
 export default theme;
--- packages/gi-site/src/components/ThemeVars/site.ts
+++ packages/gi-site/src/components/ThemeVars/site.ts
@@ -5,6 +5,7 @@ export default {
     '--hr-color': '#ddd',
     // 主要文本颜色
     '--primary-text-color': 'rgba(0, 0, 0, 0.85)',
+    '--border-color': '#ddd',
   },
   dark: {
     '--primary-color': '#363b40',
@@ -12,5 +13,6 @@ export default {
     '--hr-color': '#303030',
     // 主要文本颜色
     '--primary-text-color': 'rgba(255, 255, 255, 0.85)',
+    '--border-color': '#303030',
   },
 };
