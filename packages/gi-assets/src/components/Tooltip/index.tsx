a54f06c92017d6f5f52741c13fc310aef1aafe81
xuying.xu <xuying.xu@alibaba-inc.com>
2021-09-17 18:10:38 +0800

    add node tooltip

--- packages/gi-assets/src/components/Tooltip/index.tsx
+++ packages/gi-assets/src/components/Tooltip/index.tsx
@@ -1,3 +1,18 @@
-import EdgeTooltip from './EdgeTooltip';
-import NodeTooltip from './NodeTooltip';
-export { NodeTooltip, EdgeTooltip };
+import Component from './NodeTooltip';
+import registerMeta from './NodeTooltipMeta';
+
+/**   index.md 中解析得到默认值，也可用户手动修改 */
+
+const info = {
+  id: 'Tooltip',
+  name: '节点提示框',
+  category: 'components',
+  desc: '节点提示框',
+  cover: 'http://xxxx.jpg',
+};
+
+export default {
+  info,
+  component: Component,
+  registerMeta,
+};
