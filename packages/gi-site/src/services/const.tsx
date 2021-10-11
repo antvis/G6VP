95cbbbe4eb93a1293d5bf0bc24355b04faf1968c
jingxi.lp <jingxi.lp@mybank.cn>
2021-10-11 20:16:23 +0800

    feat set mock false

--- packages/gi-site/src/services/const.tsx
+++ packages/gi-site/src/services/const.tsx
@@ -1,4 +1,4 @@
-export const isMock = true;
+export const isMock = false;
 
 // export const SERVICE_URL_PREFIX = 'http://storehouse-afx-18554.gz00b.dev.alipay.net';
 export const SERVICE_URL_PREFIX = 'https://storehouse.test.alipay.net';
