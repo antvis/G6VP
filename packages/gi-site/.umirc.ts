2c252ad329cd6e8c83bd3242886f547cd2e79040
pomelo.lcw <pomelo.lcw@antfin.com>
2021-10-19 20:14:57 +0800

    feat:external monaco-editor 和 typescript的依赖

--- packages/gi-site/.umirc.ts
+++ packages/gi-site/.umirc.ts
@@ -43,14 +43,21 @@ export default {
     '@antv/graphin-components': 'GraphinComponents',
     '@antv/g6': 'G6',
     antd: 'antd',
+    'antd/es/*': 'antd',
     '@ant-design/charts': 'charts',
     '@ant-design/icons': 'icons',
     moment: 'moment',
+    lodash: 'lodash',
+    'lodash-es': 'lodash',
+    //资产中心那块的依赖大包
     '@alipay/alex': 'Alex',
+    typescript: 'ts',
+    'monaco-editor/esm/vs/editor/editor.api': 'monaco',
   },
   scripts: [
     'https://unpkg.com/react@17.0.2/umd/react.production.min.js',
     'https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js',
+    'https://gw.alipayobjects.com/os/lib/lodash/4.17.21/lodash.min.js',
     'https://gw.alipayobjects.com/os/lib/moment/2.29.1/moment.js',
     'https://gw.alipayobjects.com/os/lib/antd/4.16.13/dist/antd.min.js',
     'https://gw.alipayobjects.com/os/lib/antv/g6/4.3.7/dist/g6.min.js',
@@ -58,7 +65,9 @@ export default {
     'https://gw.alipayobjects.com/os/lib/antv/graphin-components/2.4.0/dist/graphin-components.min.js',
     'https://gw.alipayobjects.com/os/lib/ant-design/charts/1.2.13/dist/charts.min.js',
     'https://gw.alipayobjects.com/os/lib/ant-design/icons/4.6.4/dist/index.umd.min.js',
-
+    /** 资产中心那块的依赖大包 */
+    'https://gw.alipayobjects.com/os/lib/monaco-editor/0.27.0/min/vs/editor/editor.main.js',
+    'https://gw.alipayobjects.com/os/lib/typescript/4.4.3/lib/typescript.js',
     'https://g.alipay.com/@alipay/alex@latest/bundle/alex.global.min.js',
     'https://g.alipay.com/@alipay/alex@latest/languages/languages.global.min.js',
   ],
