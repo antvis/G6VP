213730a1e552ad85cf4c16f4f9e30fd4af82035b
pomelo-nwu <pomelo.lcw@antfin.com>
2021-10-20 11:17:58 +0800

    feat:更新gi-assets引入

--- packages/gi-site/src/services/assets.market.ts
+++ packages/gi-site/src/services/assets.market.ts
@@ -2,14 +2,14 @@
 // import * as ComponentAssets from '@alipay/graphinsight/es/components';
 // import * as ElementAssets from '@alipay/graphinsight/es/elements';
 
-import giAssets from '@alipay/gi-assets';
+import * as giAssets from '@alipay/gi-assets';
 import localforage from 'localforage';
+import { dynamicLoadModules } from '../loader';
 import { queryAssetList } from './assets';
 import { isMock } from './const';
-import { dynamicLoadModules } from '../loader'
 
 // TODO 临时方案，需要换成和 Component 一样的方案
-const { elements } = giAssets;
+const { elements } = giAssets || {};
 
 /**
  * 获取指定项目
@@ -24,23 +24,23 @@ export const queryAssets = async (id: string) => {
     {
       name: 'NodeLegend',
       // url: 'https://gw.alipayobjects.com/os/lib/alipay/gi-assets/0.3.1/dist/index.min.js'
-      url: 'http://alipay-rmsdeploy-image.cn-hangzhou.alipay.aliyun-inc.com/GraphInsight/test_legend-5e0ece9910fc7e3c9a7e2d228453e330.js'
+      url: 'http://alipay-rmsdeploy-image.cn-hangzhou.alipay.aliyun-inc.com/GraphInsight/test_legend-5e0ece9910fc7e3c9a7e2d228453e330.js',
     },
     // {
     //   name: 'NodeLegend1',
     //   url: 'https://gw.alipayobjects.com/os/lib/alipay/gi-assets/0.3.1/dist/index.min.js'
     //   // url: 'http://alipay-rmsdeploy-image.cn-hangzhou.alipay.aliyun-inc.com/GraphInsight/test_legend-5e0ece9910fc7e3c9a7e2d228453e330.js'
     // },
-  ])
-  let components = {}
+  ]);
+  let components = {};
   // let elements = {}
   if (dlm && dlm.length > 0) {
     components = dlm.reduce((acc, curr) => {
       return {
         ...acc,
-        ...curr.components
-      }
-    }, {})
+        ...curr.components,
+      };
+    }, {});
   }
 
   if (isMock) {
