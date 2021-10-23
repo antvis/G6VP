02b5da0205fe158afc3833dd2572d00cfea7ff6b
pomelo-nwu <pomelo.lcw@antfin.com>
2021-10-23 18:08:00 +0800

    feat:增加EdgeElement的元素选择

--- packages/gi-site/src/pages/Analysis/MetaPanel/StylePanel/Edge.tsx
+++ packages/gi-site/src/pages/Analysis/MetaPanel/StylePanel/Edge.tsx
@@ -5,6 +5,8 @@ import { extractDefault } from '@ali/react-datav-gui-utils';
 import { Select } from 'antd';
 import React, { useState } from 'react';
 
+const { Option } = Select;
+
 interface EdgeStylePanelProps {
   meta: any;
   data: any;
@@ -21,6 +23,11 @@ const freeExtensions = {
 const EdgeStylePanel: React.FunctionComponent<EdgeStylePanelProps> = props => {
   const { data, elements, config = { edge: { props: {} } }, dispatch } = props;
   const { edge: edgeConfig } = config;
+  const [state, setState] = useState({
+    /** 当前元素的ID */
+    elementId: edgeConfig.id,
+  });
+  const { elementId } = state;
 
   const element = elements[edgeConfig.id];
   const { configObj } = element.meta;
@@ -35,13 +42,49 @@ const EdgeStylePanel: React.FunctionComponent<EdgeStylePanelProps> = props => {
       props: rootValue,
     });
   };
-
+  const elementOptions = Object.values(elements);
+  const handleChangeShape = value => {
+    setState(preState => {
+      return {
+        ...preState,
+        elementId: value,
+      };
+    });
+    dispatch({
+      type: 'update:config:edge',
+      ...elements[value],
+    });
+  };
   const GUIComponent = React.useMemo(() => {
     return (
       <GUI configObj={configObj} valueObj={valueObj} freeExtensions={freeExtensions} onChange={handleChangeConfig} />
     );
   }, []);
-  return <>{GUIComponent}</>;
+  return (
+    <>
+      <Select
+        onChange={handleChangeShape}
+        value={elementId}
+        style={{ width: '268px', margin: '8px 16px' }}
+        size="large"
+      >
+        {elementOptions.map((c: any) => {
+          return (
+            <Option value={c.id} key={c.id}>
+              <img
+                src="https://gw.alipayobjects.com/mdn/rms_402c1a/afts/img/A*JoptTZdYEEYAAAAAAAAAAAAAARQnAQ"
+                alt=""
+                width={40}
+                height={40}
+              />
+              {c.name}
+            </Option>
+          );
+        })}
+      </Select>
+      {GUIComponent}
+    </>
+  );
 };
 
 export default EdgeStylePanel;
