d4324bb3425f3310d71c61edd2282ab1d68df97d
pomelo-nwu <pomelo.lcw@antfin.com>
2021-11-02 15:05:04 +0800

    feat:新增layout资产

---
+++ packages/gi-assets/src/layouts/Concentric/index.tsx
@@ -0,0 +1,20 @@
+import registerShape from './registerLayout';
+import registerMeta from './registerMeta';
+
+/**   index.md 中解析得到默认值，也可用户手动修改 */
+const info = {
+  id: 'Concentric',
+  options: {
+    type: 'concentric',
+  },
+  name: '同心圆布局',
+  category: 'layout',
+  desc: '',
+  cover: 'http://xxxx.jpg',
+};
+
+export default {
+  info,
+  registerShape,
+  registerMeta,
+};
