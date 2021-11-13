1b9c2d3d9a531394dd3973b44cc7b2881d3da0c7
pomelo.lcw <pomelo.lcw@antfin.com>
2021-11-13 11:45:34 +0800

    feat:修改元素label显示bug

--- packages/gi-assets/src/elements/GraphinNode/registerMeta.tsx
+++ packages/gi-assets/src/elements/GraphinNode/registerMeta.tsx
@@ -120,7 +120,7 @@ const registerMeta = context => {
           default: true,
           statusText: true,
         },
-        keyLabel: {
+        key: {
           name: '映射字段',
           type: 'select',
           useFont: true,
