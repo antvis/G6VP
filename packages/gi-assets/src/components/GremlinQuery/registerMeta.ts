f4272c6d28f29c4c2f69f423ee7db4888c603be3
pomelo-nwu <pomelo.lcw@antfin.com>
2021-11-15 14:53:37 +0800

    feat:封装通用的Wrap组件

--- packages/gi-assets/src/components/GremlinQuery/registerMeta.ts
+++ packages/gi-assets/src/components/GremlinQuery/registerMeta.ts
@@ -1,3 +1,4 @@
+import { Meta } from '../WrapContainer';
 const registerMeta = context => {
   const { services } = context;
   const serviceOptions = services.map(c => {
@@ -33,72 +34,73 @@ const registerMeta = context => {
       type: 'switch',
       default: false,
     },
-    visible: {
-      name: '默认显示',
-      type: 'switch',
-      default: false,
-    },
-    placement: {
-      name: '组件位置',
-      type: 'select',
-      default: 'LT',
-      options: [
-        {
-          value: 'LT',
-          label: '左上',
-        },
-        {
-          value: 'RT',
-          label: '右上',
-        },
-        {
-          value: 'LB',
-          label: '左下',
-        },
-        {
-          value: 'RB',
-          label: '右下',
-        },
-      ],
-      showInPanel: {
-        conditions: [['.visible', '$eq', true]],
-      },
-    },
-    offset: {
-      name: '偏移距离',
-      type: 'Offset',
-      min: 0,
-      max: 400,
-      default: [0, 0],
-      showInPanel: {
-        conditions: [['.visible', '$eq', true]],
-      },
-    },
-    /** GI原子组件 */
-    GI_CONTAINER_INDEX: {
-      name: '容器索引',
-      type: 'stepper',
-      default: 0,
-      showInPanel: {
-        conditions: [['.visible', '$eq', false]],
-      },
-    },
-    hasDivider: {
-      name: '分隔符',
-      type: 'switch',
-      default: false,
-      showInPanel: {
-        conditions: [['.visible', '$eq', false]],
-      },
-    },
-    color: {
-      name: '提示颜色',
-      type: 'fill',
-      default: '#87d068',
-      showInPanel: {
-        conditions: [['.visible', '$eq', false]],
-      },
-    },
+    ...Meta,
+    // visible: {
+    //   name: '默认显示',
+    //   type: 'switch',
+    //   default: false,
+    // },
+    // placement: {
+    //   name: '组件位置',
+    //   type: 'select',
+    //   default: 'LT',
+    //   options: [
+    //     {
+    //       value: 'LT',
+    //       label: '左上',
+    //     },
+    //     {
+    //       value: 'RT',
+    //       label: '右上',
+    //     },
+    //     {
+    //       value: 'LB',
+    //       label: '左下',
+    //     },
+    //     {
+    //       value: 'RB',
+    //       label: '右下',
+    //     },
+    //   ],
+    //   showInPanel: {
+    //     conditions: [['.visible', '$eq', true]],
+    //   },
+    // },
+    // offset: {
+    //   name: '偏移距离',
+    //   type: 'Offset',
+    //   min: 0,
+    //   max: 400,
+    //   default: [0, 0],
+    //   showInPanel: {
+    //     conditions: [['.visible', '$eq', true]],
+    //   },
+    // },
+    // /** GI原子组件 */
+    // GI_CONTAINER_INDEX: {
+    //   name: '容器索引',
+    //   type: 'stepper',
+    //   default: 0,
+    //   showInPanel: {
+    //     conditions: [['.visible', '$eq', false]],
+    //   },
+    // },
+    // hasDivider: {
+    //   name: '分隔符',
+    //   type: 'switch',
+    //   default: false,
+    //   showInPanel: {
+    //     conditions: [['.visible', '$eq', false]],
+    //   },
+    // },
+    // color: {
+    //   name: '提示颜色',
+    //   type: 'fill',
+    //   default: '#87d068',
+    //   showInPanel: {
+    //     conditions: [['.visible', '$eq', false]],
+    //   },
+    // },
   };
 };
 
--- packages/gi-assets/src/components/WrapContainer.tsx
+++ packages/gi-assets/src/components/WrapContainer.tsx
@@ -1,7 +1,70 @@
-import { SubnodeOutlined } from '@ant-design/icons';
 import { Button, Divider, Tooltip } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+export const Meta = {
+  visible: {
+    name: '默认显示',
+    type: 'switch',
+    default: true,
+  },
+  placement: {
+    name: '组件位置',
+    type: 'select',
+    default: 'LT',
+    options: [
+      {
+        value: 'LT',
+        label: '左上',
+      },
+      {
+        value: 'RT',
+        label: '右上',
+      },
+      {
+        value: 'LB',
+        label: '左下',
+      },
+      {
+        value: 'RB',
+        label: '右下',
+      },
+    ],
+  },
+  offset: {
+    name: '偏移距离',
+    type: 'Offset',
+    min: 0,
+    max: 400,
+    default: [0, 0],
+  },
+  /** GI原子组件 */
+  GI_CONTAINER_INDEX: {
+    name: '容器索引',
+    type: 'stepper',
+    default: 0,
+    // showInPanel: {
+    //   conditions: [['.visible', '$eq', false]],
+    // },
+  },
+  hasDivider: {
+    name: '分隔符',
+    type: 'switch',
+    default: false,
+  },
+  color: {
+    name: '提示颜色',
+    type: 'fill',
+    default: '#3056e3',
+  },
+};
+const defaultProps = {
+  visible: true,
+  placement: 'LT',
+  offset: [0, 0],
+  hasDivider: false,
+  color: '#3056e3',
+};
 
 export interface GIContianerProps {
   color: string;
@@ -33,9 +96,15 @@ const getPositionStyles = (placement, offset: number[]) => {
   return styles;
 };
 
-const WrapContainer = Component => {
-  return (props: any & GIContianerProps) => {
-    const { visible: defaultVisible, color, hasDivider, placement, offset } = props;
+const WrapContainer = (Component, options) => {
+  return ComponentProps => {
+    const {
+      visible: defaultVisible,
+      color,
+      hasDivider,
+      placement,
+      offset,
+    } = { ...defaultProps, ...ComponentProps } as any;
     const [visible, setVisible] = React.useState(defaultVisible);
 
     React.useEffect(() => {
@@ -52,16 +121,16 @@ const WrapContainer = Component => {
     return (
       <>
         <div onClick={onClick}>
-          <Tooltip title="Gremlin 查询" color={color} key={color}>
-            <Button type="text" icon={<SubnodeOutlined />}>
-              Gremlin 查询
+          <Tooltip title={options.title} color={color} key={color}>
+            <Button type="text" icon={options.icon}>
+              {options.showText ? options.title : ''}
             </Button>
           </Tooltip>
           {hasDivider && <Divider type="vertical" />}
         </div>
 
         {ReactDOM.createPortal(
-          <Component {...props} visible={visible} onClose={onClose} style={styles} />,
+          <Component {...ComponentProps} visible={visible} onClose={onClose} style={styles} />,
           //@ts-ignore
           document.getElementById('graphin-container'),
         )}
@@ -69,4 +138,5 @@ const WrapContainer = Component => {
     );
   };
 };
+
 export default WrapContainer;
