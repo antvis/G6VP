1849fe01fbdd81240b330fd9e0358d698bc26791
聚则 <zhanning.bzn@antgroup.com>
2021-11-15 18:11:23 +0800

    fix: 添加 Gremlin 路径查询组件

---
+++ packages/gi-assets/src/components/GremlinTemplateQuery/index.ts
@@ -0,0 +1,17 @@
+import Component from './Component';
+import registerMeta from './registerMeta';
+
+/**   index.md 中解析得到默认值，也可用户手动修改 */
+const info = {
+  id: 'GremlinTemplateQuery',
+  name: 'Gremlin 模板语句查询面板',
+  category: 'components',
+  desc: '通过 Gremlin 模板语句快速查询图数据',
+  cover: 'http://xxxx.jpg',
+};
+
+export default {
+  info,
+  component: Component,
+  registerMeta,
+};
