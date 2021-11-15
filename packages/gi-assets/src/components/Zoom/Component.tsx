7d88a51034687f50aa1d1fa805170880096f31c4
xuying.xu <xuying.xu@alibaba-inc.com>
2021-11-15 20:48:31 +0800

    add toolbar

---
+++ packages/gi-assets/src/components/Zoom/Component.tsx
@@ -0,0 +1,31 @@
+import { GraphinContext } from '@antv/graphin';
+import { Button, Divider, Modal, Tooltip } from 'antd';
+import * as React from 'react';
+import { ZoomInOutlined, ZoomOutOutlined } from '@ant-design/icons';
+
+export interface AddNode {
+  visible: boolean;
+  color: string;
+  hasDivider: boolean;
+}
+
+const Zoom: React.FunctionComponent<AddNode> = props => {
+  const { color, hasDivider } = props;
+  const { apis, graph } = React.useContext(GraphinContext);
+  const { handleZoomIn, handleZoomOut } = apis;
+
+  return (
+    <div>
+      <Tooltip title="放大" color={color} key={color}>
+        <Button type="text" icon={<ZoomInOutlined />} onClick={handleZoomOut}></Button>
+      </Tooltip>
+      {hasDivider && <Divider type="vertical" />}
+      <Tooltip title="缩小" color={color} key={color}>
+        <Button type="text" icon={<ZoomOutOutlined />} onClick={handleZoomIn}></Button>
+      </Tooltip>
+      {hasDivider && <Divider type="vertical" />}
+    </div>
+  );
+};
+
+export default Zoom;
