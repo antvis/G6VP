1473d3c02c4be894f28f2b8e802b63e9dd938533
pomelo.lcw <pomelo.lcw@antfin.com>
2021-11-15 14:53:37 +0800

    feat:封装通用的Wrap组件

--- packages/gi-assets/src/components/GremlinQuery/Component.tsx
+++ packages/gi-assets/src/components/GremlinQuery/Component.tsx
@@ -1,9 +1,9 @@
 import { CloseOutlined, SubnodeOutlined } from '@ant-design/icons';
 import { GraphinContext } from '@antv/graphin';
-import { Button, Col, Divider, Row, Tooltip } from 'antd';
+import { Button, Col, Divider, Row } from 'antd';
 import classNames from 'classnames';
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import WrapContainer from '../WrapContainer';
 import './index.less';
 
 export interface IGremlinQueryProps {
@@ -114,71 +114,9 @@ const GremlinQueryPanel: React.FC<IGremlinQueryProps> = ({
     </div>
   );
 };
-export interface GIContianerProps {
-  color: string;
-  hasDivider: boolean;
-  placement: 'LT' | 'RT' | 'LB' | 'RB';
-  offset: [number, number];
-}
-const getPositionStyles = (placement, offset: number[]) => {
-  const styles: { [key: string]: string } = {
-    position: 'absolute',
-  };
-  const [offsetX, offsetY] = offset;
-  if (placement === 'RT') {
-    styles.right = `${offsetX}px`;
-    styles.top = `${offsetY}px`;
-  }
-  if (placement === 'LT') {
-    styles.left = `${offsetX}px`;
-    styles.top = `${offsetY}px`;
-  }
-  if (placement === 'LB') {
-    styles.left = `${offsetX}px`;
-    styles.bottom = `${offsetY}px`;
-  }
-  if (placement === 'RB') {
-    styles.right = `${offsetX}px`;
-    styles.bottom = `${offsetY}px`;
-  }
-  return styles;
-};
 
-const WrapGIContainer = Component => {
-  return (props: IGremlinQueryProps & GIContianerProps) => {
-    const { visible: defaultVisible, color, hasDivider, placement, offset } = props;
-    const [visible, setVisible] = React.useState(defaultVisible);
-
-    React.useEffect(() => {
-      setVisible(defaultVisible);
-    }, [defaultVisible]);
-    const onClick = () => {
-      setVisible(!visible);
-    };
-    const onClose = () => {
-      setVisible(false);
-    };
-    const styles = getPositionStyles(placement, offset);
-
-    return (
-      <>
-        <div onClick={onClick}>
-          <Tooltip title="Gremlin 查询" color={color} key={color}>
-            <Button type="text" icon={<SubnodeOutlined />}>
-              Gremlin 查询
-            </Button>
-          </Tooltip>
-          {hasDivider && <Divider type="vertical" />}
-        </div>
-
-        {ReactDOM.createPortal(
-          <Component {...props} visible={visible} onClose={onClose} style={styles} />,
-          //@ts-ignore
-          document.getElementById('graphin-container'),
-        )}
-      </>
-    );
-  };
-};
-
-export default WrapGIContainer(GremlinQueryPanel);
+export default WrapContainer(GremlinQueryPanel, {
+  icon: <SubnodeOutlined />,
+  title: ' Gremlin 查询',
+  showText: true,
+});
