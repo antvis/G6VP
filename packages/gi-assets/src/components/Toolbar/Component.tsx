8e6ac80196be378769e8c0b12bf48727d0a99fb6
pomelo.lcw <pomelo.lcw@antfin.com>
2021-11-16 11:24:59 +0800

    feat:toolbar添加样式

--- packages/gi-assets/src/components/Toolbar/Component.tsx
+++ packages/gi-assets/src/components/Toolbar/Component.tsx
@@ -1,19 +1,24 @@
 import { Toolbar } from '@antv/graphin-components';
 import * as React from 'react';
+import { getPositionStyles } from '../WrapContainer';
 
 type direction = 'vertical' | 'horizontal';
 export interface ToolbarProps {
   direction?: direction;
   components?: any;
   assets?: any;
+  placement: string;
+  offset: number[];
 }
 
 const ToolbarA: React.FunctionComponent<ToolbarProps> = props => {
-  const { direction = 'horizontal', components, assets } = props;
+  const { direction = 'horizontal', components, assets, placement, offset } = props;
+  const positionStyles = getPositionStyles(placement, offset);
+
   const sortedComponents = components.sort((a, b) => a.props?.GI_CONTAINER_INDEX - b.props?.GI_CONTAINER_INDEX);
   return (
     <>
-      <Toolbar direction={direction}>
+      <Toolbar direction={direction} style={positionStyles}>
         {sortedComponents.map(item => {
           if (!item) {
             return null;
