012773d7e63ff584c4728a43fd31a2cc0aa4848c
pomelo-nwu <pomelo.lcw@antfin.com>
2021-12-02 20:29:47 +0800

    feat:优化

--- packages/gi-assets/src/components/FitView/registerMeta.ts
+++ packages/gi-assets/src/components/FitView/registerMeta.ts
@@ -1,8 +1,10 @@
 import { deepClone, GIAC_METAS } from '../const';
 
 const metas = deepClone(GIAC_METAS);
-metas.GIAC.children.title.default = '自适应';
-metas.GIAC.children.icon.default = 'icon-clear';
+metas.GIAC.children.title.default = '1:1';
+metas.GIAC.children.icon.default = 'icon-fit-view';
+metas.GIAC.children.isShowTitle.default = false;
+metas.GIAC.children.tooltipPlacement.default = 'right';
 
 export default () => {
   return metas;
--- packages/gi-assets/src/components/ZoomIn/registerMeta.ts
+++ packages/gi-assets/src/components/ZoomIn/registerMeta.ts
@@ -2,7 +2,9 @@ import { deepClone, GIAC_METAS } from '../const';
 
 const metas = deepClone(GIAC_METAS);
 metas.GIAC.children.title.default = '放大';
-metas.GIAC.children.icon.default = 'icon-zoom-in';
+metas.GIAC.children.icon.default = 'icon-zoomin';
+metas.GIAC.children.isShowTitle.default = false;
+metas.GIAC.children.tooltipPlacement.default = 'right';
 
 export default () => {
   return metas;
