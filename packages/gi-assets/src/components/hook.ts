bdfba9cc314282dcbe4ef29fa0b40f4cb7594047
pomelo-nwu <pomelo.lcw@antfin.com>
2021-12-24 18:24:58 +0800

    feat:新增Hook

---
+++ packages/gi-assets/src/components/hook.ts
@@ -0,0 +1,16 @@
+import { GraphinContext } from '@antv/graphin';
+
+export const useServiceContext = serviceId => {
+  if (!serviceId) {
+    console.warn('not found serviceId', serviceId);
+    return null;
+  }
+  //@ts-ignore
+  const { services } = GraphinContext;
+  const { service } = services.find(s => s.id === serviceId);
+  if (!service) {
+    console.warn('NodeEdgeAttrs Component need a serviceId', serviceId);
+    return null;
+  }
+  return service;
+};
