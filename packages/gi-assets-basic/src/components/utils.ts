2bc3acf80ed0c184b57357d3025476738de3bc23
pomelo-nwu <czynwu@outlook.com>
2022-01-07 14:38:40 +0800

    chore:fix bugs

--- packages/gi-assets-basic/src/components/utils.ts
+++ packages/gi-assets-basic/src/components/utils.ts
@@ -25,3 +25,52 @@ export const getPositionStyles = (placement, offset: number[]) => {
   }
   return styles;
 };
+
+/** 数据去重 */
+export const uniqueElementsBy = (arr: any[], fn: (arg0: any, arg1: any) => any) =>
+  arr.reduce((acc, v) => {
+    if (!acc.some((x: any) => fn(v, x))) acc.push(v);
+    return acc;
+  }, []);
+
+/**
+ *
+ * @param data 画布中的图数据
+ * @param responseData 扩展出来的图数据
+ * @returns
+ */
+export const handleExpand = (data, responseData) => {
+  const { nodes, edges } = responseData;
+  return {
+    nodes: uniqueElementsBy([...data.nodes, ...nodes], (a, b) => {
+      return a.id === b.id;
+    }),
+    edges: uniqueElementsBy([...data.edges, ...edges], (a, b) => {
+      return a.source === b.source && a.target === b.target;
+    }),
+  };
+};
+
+/**
+ *
+ * @param data 画布中的图数据
+ * @param responseData 需要收起的图数据
+ * @returns
+ */
+export const handleCollaspe = (data, responseData) => {
+  const nodeIds = responseData.nodes.map(c => c.id);
+  const edgeIds = responseData.edges.map(c => `${c.source}-${c.target}`);
+  const nodes = data.nodes.filter(c => {
+    return nodeIds.indexOf(c.id) === -1;
+  });
+  const edges = data.edges.filter(c => {
+    const id = `${c.source}-${c.target}`;
+    return edgeIds.indexOf(id) === -1;
+  });
+  console.log(nodes, edges);
+
+  return {
+    nodes,
+    edges,
+  };
+};
