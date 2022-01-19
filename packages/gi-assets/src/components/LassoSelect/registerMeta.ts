d7100d2b7297bc9fdfda46556b1185c92048cf9f
pomelo-nwu <czynwu@outlook.com>
2022-01-19 14:23:53 +0800

    feat:将基础组件从gi-assets中移除

--- packages/gi-assets/src/components/LassoSelect/registerMeta.ts
+++
@@ -1,9 +0,0 @@
-import { deepClone, GIAC_METAS } from '../const';
-
-const metas = deepClone(GIAC_METAS);
-metas.GIAC.children.title.default = '拉索圈选';
-metas.GIAC.children.icon.default = 'icon-lasso';
-
-export default () => {
-  return metas;
-};
