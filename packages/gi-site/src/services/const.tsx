8b060c79b8d613f5cf7a978b9815d4bd802d566a
pomelo-nwu <czynwu@outlook.com>
2022-03-16 13:48:01 +0800

    feat:增加环境变量

--- packages/gi-site/src/services/const.tsx
+++ packages/gi-site/src/services/const.tsx
@@ -1,4 +1,4 @@
-export const isMock = false;
+export const isMock = window.localStorage.getItem('GI_SERVER_ENV') === 'LOCAL' ? true : false;
 
 // export const SERVICE_URL_PREFIX = 'http://storehouse-afx-18554.gz00b.dev.alipay.net';
 export const SERVICE_URL_PREFIX = 'https://storehouse.test.alipay.net';
