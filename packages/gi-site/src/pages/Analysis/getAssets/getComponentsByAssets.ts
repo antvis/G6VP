6c9aa3cb28398b72c46c00f02633ff4fa39ce8fa
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-03-16 23:12:36 +0800

    feat:反向解析service instance

--- packages/gi-site/src/pages/Analysis/getAssets/getComponentsByAssets.ts
+++ packages/gi-site/src/pages/Analysis/getAssets/getComponentsByAssets.ts
@@ -2,6 +2,26 @@ import { extractDefault } from '@ali/react-datav-gui-utils';
 import type { TypeAssetInfo } from './typing';
 import { getKeysByData } from './utils';
 
+var stringify = function (obj, prop) {
+  var placeholder = '____PLACEHOLDER____';
+  var fns = [];
+  var json = JSON.stringify(
+    obj,
+    function (key, value) {
+      if (typeof value === 'function') {
+        fns.push(value);
+        return placeholder;
+      }
+      return value;
+    },
+    2,
+  );
+  json = json.replace(new RegExp('"' + placeholder + '"', 'g'), function (_) {
+    return fns.shift();
+  });
+  return 'this["' + prop + '"] = ' + json + ';';
+};
+
 /**
  *
  * @param assets 服务端拿到的资产: Components
@@ -9,6 +29,8 @@ import { getKeysByData } from './utils';
  * @returns
  */
 const getComponentsByAssets = (assets, data, services, config) => {
+  let MOCK_SERVICES = [];
+  let MOCK_SERVICES_CONFIG = [];
   const GI_CONTAINER_INDEXS = Object.values(assets)
     .filter((item: any) => {
       const info = ((item && item.info) || {}) as TypeAssetInfo;
@@ -42,6 +64,7 @@ const getComponentsByAssets = (assets, data, services, config) => {
           return {};
         },
         Component,
+        mockServices,
         info = {},
       } = component;
       const keys = getKeysByData(data, 'node');
@@ -50,6 +73,20 @@ const getComponentsByAssets = (assets, data, services, config) => {
       /** 默认的配置值 */
       const defaultProps = extractDefault({ config: configObj, value: {} });
       const { id, name, category } = info;
+      if (mockServices) {
+        const sers = mockServices();
+        MOCK_SERVICES = [...MOCK_SERVICES, ...sers];
+        const sers_config = sers.map(c => {
+          return {
+            id: c.id,
+            mode: 'MOCK',
+            name: c.id,
+            //@ts-ignore
+            content: stringify(c.service),
+          };
+        });
+        MOCK_SERVICES_CONFIG = [...MOCK_SERVICES_CONFIG, ...sers_config];
+      }
       return {
         id,
         name,
@@ -67,6 +104,6 @@ const getComponentsByAssets = (assets, data, services, config) => {
       return c.id !== 'NOT_FOUND';
     });
 
-  return components;
+  return { components, mockServices: MOCK_SERVICES, mockServicesConfig: MOCK_SERVICES_CONFIG };
 };
 export default getComponentsByAssets;
