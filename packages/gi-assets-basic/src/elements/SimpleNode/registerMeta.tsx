7a61bcc48ec6d36b9838b2442936274590cd02c5
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-03-20 12:59:42 +0800

    feat:add SimpleNode and SimpleEdge

---
+++ packages/gi-assets-basic/src/elements/SimpleNode/registerMeta.tsx
@@ -0,0 +1,13 @@
+const registerMeta = context => {
+  const { data, keys = ['id', 'type'] } = context;
+  const options = keys.map(c => {
+    return {
+      value: c,
+      label: c,
+    };
+  });
+
+  return {};
+};
+
+export default registerMeta;
