79533e44e04cd3bf42efeffb111eac97ec5557b1
聚则 <zhanning.bzn@antgroup.com>
2022-03-23 15:18:38 +0800

    fix: 组件封装

--- packages/gi-assets-basic/src/components/StyleSetting/Component.tsx
+++ packages/gi-assets-basic/src/components/StyleSetting/Component.tsx
@@ -1,9 +1,7 @@
-import { GroupContainer } from '@alipay/gi-common-components';
+import { CommonStyleSetting } from '@alipay/gi-common-components';
 import { useContext } from '@alipay/graphinsight';
-import FormRender, { useForm } from 'form-render';
 import React from 'react';
 import { NodeConfig } from '../../elements/SimpleNode/registerTransform';
-import IconSelector from './IconSelector';
 import { schema } from './registerMeta';
 
 export type NodesConfig = {
@@ -57,15 +55,7 @@ const data = {
   ],
 };
 
-const getTransformOptions = options => {
-  // TODO :替换成getTransByTemplate
-  return () => {};
-};
-
-let preStyleGroup: any = [];
-
 const StyleSetting: React.FunctionComponent<StyleSettingProps> = ({ shapeOptions, elementType }) => {
-  const form = useForm();
   const { updateContext } = useContext();
 
   /**
@@ -73,19 +63,8 @@ const StyleSetting: React.FunctionComponent<StyleSettingProps> = ({ shapeOptions
    * @param current
    * @param all
    */
-  const handleChange = (current, all, groupIndex) => {
-    if (preStyleGroup[groupIndex]) {
-      preStyleGroup[groupIndex].config = all;
-    } else {
-      // 不设置分组规则
-      preStyleGroup[groupIndex] = {
-        config: all,
-        groupName: `样式配置分组${groupIndex + 1}`,
-        groupId: 'default-group',
-      };
-    }
-    console.log('preStyleGroup', preStyleGroup);
-    const nodesConfig: NodesConfig = preStyleGroup.map(c => {
+  const handleChange = styleGroups => {
+    const nodesConfig: NodesConfig = styleGroups.map(c => {
       return {
         id: 'SimpleNode',
         props: c.config as NodeConfig,
@@ -105,39 +84,7 @@ const StyleSetting: React.FunctionComponent<StyleSettingProps> = ({ shapeOptions
     });
   };
 
-  const handleGroupChange = (current, all) => {
-    const resultGroup: any = [];
-    for (const group of all.groups) {
-      // 从 preStyleGroup 中过滤出相同 ID 的对象，进行 merge
-      const currentGroup = preStyleGroup.find(pg => pg.groupId === group.groupId);
-      if (currentGroup) {
-        // 进行 merge
-        const result = Object.assign({}, currentGroup, group);
-        resultGroup.push(result);
-      } else {
-        resultGroup.push(group);
-      }
-    }
-    preStyleGroup = resultGroup;
-  };
-
-  return (
-    <GroupContainer data={data.nodes} valuesChange={handleGroupChange}>
-      {groupIndex => {
-        return (
-          <FormRender
-            widgets={{ iconSelector: IconSelector }}
-            form={form}
-            displayType="row"
-            allCollapsed={true}
-            // debounceInput={true}
-            schema={schema as any}
-            onValuesChange={(current, all) => handleChange(current, all, groupIndex)}
-          />
-        );
-      }}
-    </GroupContainer>
-  );
+  return <CommonStyleSetting schema={schema} onChange={handleChange} data={data} elementType="node" />;
 };
 
 export default StyleSetting;
