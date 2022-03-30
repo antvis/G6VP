53b61fcf86aa8dfee2b9e699cde62a5d58dbabbb
yunyi <hujinpu.hjp@alibaba-inc.com>
2022-03-30 20:44:30 +0800

    feat: add Copyright asset

---
+++ packages/gi-assets-basic/src/components/Copyright/Component.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './index.less'
+import { getPositionStyles } from '../WrapContainer';
+
+export interface CopyrightProps {
+  imageUrl: string;
+  width: number;
+  height: number;
+  placement: string;
+  offset: number[];
+}
+
+const Copyright: React.FunctionComponent<CopyrightProps> = props => {
+  const { imageUrl, width, height, placement, offset } = props;
+  const positionStyles = getPositionStyles(placement, offset);
+
+  return (
+    <div className='gi-copyright' style={positionStyles}>
+      <img src={`${imageUrl}`} alt="版权图片" style={{
+        width: `${width}px`,
+        height: `${height}px`
+      }} />
+    </div>
+  );
+};
+
+export default Copyright;
