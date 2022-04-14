cdc080d6a6c985c065492dbe6567bfcf526b11c7
pomelo-nwu <czynwu@outlook.com>
2022-04-14 15:18:25 +0800

    feat: 解决bug

--- packages/gi-assets-basic/src/elements/DonutNode/registerMeta.tsx
+++ packages/gi-assets-basic/src/elements/DonutNode/registerMeta.tsx
@@ -1,16 +1,14 @@
 const registerMeta = context => {
   try {
-    const { data, keys } = context;
-    const firstNode = data.nodes[0]?.data;
-    const numberKeys = Object.keys(firstNode).filter(key => {
-      return typeof firstNode[key] === 'number';
-    });
+    const { keys } = context;
+
+    const numberKeys = keys.filter(key => key.type === 'number').map(c => c.id);
     const numberOptions = numberKeys.map(c => {
       return { label: c, value: c };
     });
+
     return {
       type: 'object',
-      collapsed: false,
       properties: {
         donut: {
           title: '环展示',
@@ -27,11 +25,13 @@ const registerMeta = context => {
         label: {
           title: '文本',
           type: 'array',
-          //todo: 显示文本属性根据 data 生成
           enum: keys.map(c => {
-            return { label: c, value: c };
+            return {
+              label: `${c.id} (${c.type})`,
+              value: c.id,
+            };
           }),
-          default: keys,
+          default: ['id'],
           'x-decorator': 'FormItem',
           'x-component': 'Select',
           'x-component-props': {
