5618b382828a7380c82fda924814a6e3e458a185
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-04-18 20:57:58 +0800

    feat: 移除graphinComponents的依赖

--- packages/gi-assets-basic/.umirc.ts
+++ packages/gi-assets-basic/.umirc.ts
@@ -6,7 +6,7 @@ export default {
     react: 'React',
     'react-dom': 'ReactDOM',
     '@antv/graphin': 'Graphin',
-    '@antv/graphin-components': 'GraphinComponents',
+
     '@antv/g6': 'G6',
     antd: 'antd',
     '@ant-design/charts': 'charts',
@@ -21,13 +21,11 @@ export default {
     'https://gw.alipayobjects.com/os/lib/antd/4.16.13/dist/antd.min.js',
     'https://gw.alipayobjects.com/os/lib/antv/g6/4.6.4/dist/g6.min.js',
     'https://gw.alipayobjects.com/os/lib/antv/graphin/2.6.4/dist/graphin.min.js',
-    'https://gw.alipayobjects.com/os/lib/antv/graphin-components/2.4.0/dist/graphin-components.min.js',
     'https://gw.alipayobjects.com/os/lib/ant-design/charts/1.2.13/dist/charts.min.js',
     'https://gw.alipayobjects.com/os/lib/ant-design/icons/4.6.4/dist/index.umd.min.js',
   ],
   styles: [
     'https://gw.alipayobjects.com/os/lib/antd/4.16.13/dist/antd.min.css',
     'https://gw.alipayobjects.com/os/lib/antv/graphin/2.6.4/dist/index.css',
-    'https://gw.alipayobjects.com/os/lib/antv/graphin-components/2.4.0/dist/index.css',
   ],
 };
--- packages/gi-site/.umirc.ts
+++ packages/gi-site/.umirc.ts
@@ -55,7 +55,6 @@ export default {
     react: 'React',
     'react-dom': 'ReactDOM',
     '@antv/graphin': 'Graphin',
-    '@antv/graphin-components': 'GraphinComponents',
     '@antv/g6': 'G6',
     antd: 'antd',
     'antd/es/*': 'antd',
@@ -73,7 +72,6 @@ export default {
     'https://gw.alipayobjects.com/os/lib/antd/4.16.13/dist/antd.min.js',
     'https://gw.alipayobjects.com/os/lib/antv/g6/4.6.3/dist/g6.min.js',
     'https://gw.alipayobjects.com/os/lib/antv/graphin/2.6.2/dist/graphin.min.js',
-    'https://gw.alipayobjects.com/os/lib/antv/graphin-components/2.4.0/dist/graphin-components.min.js',
 
     /** GI */
     ...localScripts,
@@ -82,8 +80,7 @@ export default {
   ],
   styles: [
     'https://gw.alipayobjects.com/os/lib/antd/4.16.13/dist/antd.min.css',
-    'https://gw.alipayobjects.com/os/lib/antv/graphin/2.6.2/dist/index.css',
-    'https://gw.alipayobjects.com/os/lib/antv/graphin-components/2.4.0/dist/index.css',
+    'https://gw.alipayobjects.com/os/lib/antv/graphin/2.6.4/dist/index.css',
     'https://g.alipay.com/@alipay/alex@1.5.2/bundle/alex.global.min.css',
   ],
   analyze: {
