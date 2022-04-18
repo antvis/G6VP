7b91b25bac1e0de436bdef64ac9d4040e59adc3b
pomelo-nwu <czynwu@outlook.com>
2022-04-18 18:22:19 +0800

    feat: 修复basic assets的docs报错

--- packages/gi-assets-basic/.umirc.ts
+++ packages/gi-assets-basic/.umirc.ts
@@ -19,15 +19,15 @@ export default {
     'https://gw.alipayobjects.com/os/lib/lodash/4.17.21/lodash.min.js',
     'https://gw.alipayobjects.com/os/lib/moment/2.29.1/moment.js',
     'https://gw.alipayobjects.com/os/lib/antd/4.16.13/dist/antd.min.js',
-    'https://gw.alipayobjects.com/os/lib/antv/g6/4.5.0/dist/g6.min.js',
-    'https://gw.alipayobjects.com/os/lib/antv/graphin/2.4.9/dist/graphin.min.js',
+    'https://gw.alipayobjects.com/os/lib/antv/g6/4.6.4/dist/g6.min.js',
+    'https://gw.alipayobjects.com/os/lib/antv/graphin/2.6.4/dist/graphin.min.js',
     'https://gw.alipayobjects.com/os/lib/antv/graphin-components/2.4.0/dist/graphin-components.min.js',
     'https://gw.alipayobjects.com/os/lib/ant-design/charts/1.2.13/dist/charts.min.js',
     'https://gw.alipayobjects.com/os/lib/ant-design/icons/4.6.4/dist/index.umd.min.js',
   ],
   styles: [
     'https://gw.alipayobjects.com/os/lib/antd/4.16.13/dist/antd.min.css',
-    'https://gw.alipayobjects.com/os/lib/antv/graphin/2.4.9/dist/index.css',
+    'https://gw.alipayobjects.com/os/lib/antv/graphin/2.6.4/dist/index.css',
     'https://gw.alipayobjects.com/os/lib/antv/graphin-components/2.4.0/dist/index.css',
   ],
 };
