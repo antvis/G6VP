752f738ae4f6589b502e68d2ffff8b42220eed76
pomelo-nwu <czynwu@outlook.com>
2022-04-19 07:17:43 +0800

    feat: add gi-assets-advance

--- packages/gi-assets/src/index.ts
+++ packages/gi-assets/src/index.ts
@@ -1,10 +1,4 @@
 import * as components from './components';
 import * as elements from './elements';
-import * as layouts from './layouts';
-import getServicesByAssets from './services';
 
-const utils = {
-  getServicesByAssets,
-};
-
-export { components, elements, layouts, utils };
+export { components, elements };
