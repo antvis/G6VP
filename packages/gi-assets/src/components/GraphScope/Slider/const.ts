a41ecc322b75e9b37547a53d1822ccf7922128c1
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-04-19 09:39:14 +0800

    feat: remove dump code

--- packages/gi-assets/src/components/GraphScope/Slider/const.ts
+++
@@ -1,5 +0,0 @@
-export const imgSrcPrefix = 'http://47.57.243.56:8080/images';
-
-export const githubPrefix = 'https://avatars.githubusercontent.com';
-
-export const getSpliceUrl = url => url.replace(githubPrefix, imgSrcPrefix);
