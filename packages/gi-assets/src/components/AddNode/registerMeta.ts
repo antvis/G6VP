586c8a48c61ff6d364456942c86242daee9a81d0
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-04-19 09:25:12 +0800

    chore: update AddNode

--- packages/gi-assets/src/components/AddNode/registerMeta.ts
+++ packages/gi-assets/src/components/AddNode/registerMeta.ts
@@ -1,8 +1,8 @@
-import { deepClone, GIAC_METAS } from '../const';
+import { deepClone, GIAC_CONTENT_METAS } from '../const';
 
-const metas = deepClone(GIAC_METAS);
-metas.GIAC.children.title.default = '添加节点';
-metas.GIAC.children.icon.default = 'icon-plus';
+const metas = deepClone(GIAC_CONTENT_METAS);
+metas.GIAC_CONTENT.children.title.default = '添加节点';
+metas.GIAC_CONTENT.children.icon.default = 'icon-plus';
 
 export default () => {
   return metas;
