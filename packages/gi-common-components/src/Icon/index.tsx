cda1083e8250bc683833d8971780fcf0cc011b25
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-05-07 07:42:58 +0800

    feat: 修改样式

---
+++ packages/gi-common-components/src/Icon/index.tsx
@@ -0,0 +1,8 @@
+import { createFromIconfontCN } from '@ant-design/icons';
+
+const Icon = createFromIconfontCN({
+  // 资源地址：https://www.iconfont.cn/manage/index?spm=a313x.7781069.1998910419.20&manage_type=myprojects&projectId=3381398&keyword=&project_type=&page=
+  scriptUrl: 'https://at.alicdn.com/t/font_3381398_tvblwk2a2vd.js', // 在 iconfont.cn 上生成
+});
+
+export default Icon;
--- packages/gi-common-components/src/index.ts
+++ packages/gi-common-components/src/index.ts
@@ -1,10 +1,9 @@
 export { default as CommonStyleSetting } from './CommonStyleSetting';
-export { default as GroupContainer } from './GroupContainer';
-export { default as RadiusTabs } from './RadiusTabs';
-export { default as Utils } from './Utils';
-
 export { default as ColorInput } from './CommonStyleSetting/ColorInput';
 export { default as IconPicker } from './CommonStyleSetting/IconPicker';
 export { default as IconSelector } from './CommonStyleSetting/IconSelector';
-
 export * from './FormilyForm';
+export { default as GroupContainer } from './GroupContainer';
+export { default as Icon } from './Icon/index';
+export { default as RadiusTabs } from './RadiusTabs';
+export { default as Utils } from './Utils';
