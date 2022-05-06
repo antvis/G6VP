29728ab82f86798570568682b0f9761685476903
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-05-06 22:14:03 +0800

    feat: 更新资源地址

--- packages/gi/src/components/Icon.tsx
+++ packages/gi/src/components/Icon.tsx
@@ -1,7 +1,8 @@
 import { createFromIconfontCN } from '@ant-design/icons';
 
 const Icon = createFromIconfontCN({
-  scriptUrl: 'https://at.alicdn.com/t/font_3327296_uv87arfs50c.js', //'https://at.alicdn.com/t/font_2981956_aelfhedtkje.js', // 在 iconfont.cn 上生成
+  // 资源地址：https://www.iconfont.cn/manage/index?spm=a313x.7781069.1998910419.20&manage_type=myprojects&projectId=3381398&keyword=&project_type=&page=
+  scriptUrl: 'https://at.alicdn.com/t/font_3381398_tvblwk2a2vd.js', // 在 iconfont.cn 上生成
 });
 
 export default Icon;
