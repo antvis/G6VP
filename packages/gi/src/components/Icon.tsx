f80e5bbcd291588973acbe85911a01fa35673c40
pomelo-nwu <czynwu@outlook.com>
2022-05-07 08:53:41 +0800

    feat: 优化样式

--- packages/gi/src/components/Icon.tsx
+++ packages/gi/src/components/Icon.tsx
@@ -2,7 +2,7 @@ import { createFromIconfontCN } from '@ant-design/icons';
 
 const Icon = createFromIconfontCN({
   // 资源地址：https://www.iconfont.cn/manage/index?spm=a313x.7781069.1998910419.20&manage_type=myprojects&projectId=3381398&keyword=&project_type=&page=
-  scriptUrl: 'https://at.alicdn.com/t/font_3381398_tvblwk2a2vd.js', // 在 iconfont.cn 上生成
+  scriptUrl: 'https://at.alicdn.com/t/font_3381398_ci8s8e25h3u.js', // 在 iconfont.cn 上生成
 });
 
 export default Icon;
