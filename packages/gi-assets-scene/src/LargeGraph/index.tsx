07bd1095ce0b92e63a247bb2d2626ff376d301d6
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-05-08 21:57:51 +0800

    feat: 优化schema

--- packages/gi-assets-scene/src/LargeGraph/index.tsx
+++ packages/gi-assets-scene/src/LargeGraph/index.tsx
@@ -1,17 +1,7 @@
 import Component from './Component';
+import info from './info';
 import registerMeta from './registerMeta';
-
 /**   index.md 中解析得到默认值，也可用户手动修改 */
-
-const info = {
-  id: 'LargeGraph',
-  name: '3D大图',
-  category: 'scene-analysis',
-  desc: '启用3D视图，可右键节点交互',
-  icon: 'icon-3d',
-  cover: 'http://xxxx.jpg',
-  type: 'GIAC',
-};
 
 export default {
   info,
