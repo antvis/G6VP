be84f2007962d2086afb87f53701600a6ceb0f84
pomelo-nwu <czynwu@outlook.com>
2022-05-12 11:07:30 +0800

    feat: 新增sheetbar的面板配置

--- packages/gi-assets-advance/src/components/Sheetbar/registerMeta.ts
+++ packages/gi-assets-advance/src/components/Sheetbar/registerMeta.ts
@@ -9,7 +9,32 @@
 
 const registerMeta = () => {
   return {
-    // ...metas,
+    placement: {
+      title: '放置方位',
+      type: 'string',
+      'x-decorator': 'FormItem',
+      'x-component': 'Select',
+      'x-component-props': {
+        options: [
+          {
+            value: 'top',
+            label: '顶部',
+          },
+          {
+            value: 'bottom',
+            label: '底部',
+          },
+        ],
+      },
+      default: 'bottom',
+    },
+    height: {
+      title: '页签高度',
+      type: 'number',
+      'x-decorator': 'FormItem',
+      'x-component': 'NumberPicker',
+      default: 40,
+    },
   };
 };
 
---
+++ packages/gi-assets-advance/src/components/Sheetbar/utils.ts
@@ -0,0 +1,43 @@
+export const getStyles = (height: number, placement: 'top' | 'bottom'): { [key: string]: React.CSSProperties } => {
+  if (placement === 'top') {
+    return {
+      container: {
+        position: 'absolute',
+        top: `${height}px`,
+        height: `calc( 100% - ${height}px )`,
+      },
+      sheetbar: {
+        position: 'absolute',
+        display: 'flex',
+        top: '0px',
+        left: '0px',
+        right: '0px',
+        height: `${height}px`,
+        lineHeight: `${height}px`,
+        background: '#fff',
+        border: '1px solid rgb(250, 250, 250)',
+        zIndex: 99,
+      },
+    };
+  }
+
+  return {
+    container: {
+      position: 'unset',
+      top: `unset`,
+      height: `calc( 100% - ${height}px )`,
+    },
+    sheetbar: {
+      display: 'flex',
+      position: 'absolute',
+      bottom: '0px',
+      left: '0px',
+      right: '0px',
+      height: `${height}px`,
+      lineHeight: `${height}px`,
+      background: '#fff',
+      border: '1px solid rgb(250, 250, 250)',
+      zIndex: 99,
+    },
+  };
+};
