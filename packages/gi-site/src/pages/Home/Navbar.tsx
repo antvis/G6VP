17a7ede00c45e98f4d3f39bc00dc2180b64a6d44
pomelo-nwu <czynwu@outlook.com>
2022-05-21 08:13:25 +0800

    feat: 在site中更新首页的渲染

---
+++ packages/gi-site/src/pages/Home/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface NavbarProps {}
+
+const Navbar: React.FunctionComponent<NavbarProps> = props => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
