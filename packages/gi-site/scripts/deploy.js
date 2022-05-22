821fa215aebff0802424353bdde2179a908cc82f
pomelo-nwu <czynwu@outlook.com>
2022-05-22 18:26:57 +0800

    feat: 新增copy home.html 的脚本

--- packages/gi-site/scripts/deploy.js
+++ packages/gi-site/scripts/deploy.js
@@ -1,16 +1,38 @@
 var ghpages = require('gh-pages');
 const fs = require('fs');
+var fsExtra = require('fs-extra');
 const path = require('path');
 
 const siteUrl = 'graphinsight.antv.vision';
-
+/** 修改CNAME  */
 fs.writeFile(path.resolve(__dirname, '../dist', 'CNAME'), siteUrl, err => {
   if (err) {
     throw err;
   }
   console.log('The CNAME was succesfully created!');
 });
+/** 从gi-portal中拷贝产物*/
 
+fsExtra.copy(
+  path.resolve(__dirname, '../../gi-portal/dist/index.html'),
+  path.resolve(__dirname, '../dist/home.html'),
+  { overwrite: true },
+  function (err) {
+    if (err) return console.error(err);
+    console.log('copy home.html success!');
+  },
+);
+fsExtra.copy(
+  path.resolve(__dirname, '../../gi-portal/dist/'),
+  path.resolve(__dirname, '../dist/'),
+  { overwrite: false },
+  function (err) {
+    if (err) return console.error(err);
+    console.log('copy home.js success!');
+  },
+);
+
+/** 发布 */
 ghpages.publish(
   './dist',
   {
--- packages/gi-site/scripts/process.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs-extra');
-
-fs.copy('./src/pages/Home/index.html', './dist/home.html', { overwrite: true }, function (err) {
-  if (err) return console.error(err);
-  console.log('copy home.html success!');
-});
