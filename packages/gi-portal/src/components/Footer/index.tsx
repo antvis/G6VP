e0b53673db64d34acd1126e453205d638fc8bda0
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-05-22 18:25:28 +0800

    feat: 修改首页样式

---
+++ packages/gi-portal/src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import './index.less';
+interface FooterProps {}
+
+const Footer: React.FunctionComponent<FooterProps> = props => {
+  return <div className="gi-footer">AntV ©2022 Created by GraphInsight Team</div>;
+};
+
+export default Footer;
