1404faaba20a32c9c980c4ae34aaa89fd4adbaa7
pomelo-nwu <czynwu@outlook.com>
2022-05-24 08:15:52 +0800

    feat: 新增一键洞察

---
+++ packages/gi-assets-analysis/src/QuickInsight/info.ts
@@ -0,0 +1,11 @@
+const info = {
+  id: 'QuickInsight',
+  name: '一键洞察',
+  desc: '基于 AntV/AVA 的统计数据智能洞察资产',
+  icon: 'icon-bulb',
+  cover: 'http://xxxx.jpg',
+  category: 'data-analysis',
+  type: 'GIAC',
+};
+
+export default info;
