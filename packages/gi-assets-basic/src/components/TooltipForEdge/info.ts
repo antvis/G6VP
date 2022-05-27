3ffda9f5a902cc7aa4c0a8dd103fa5821f4dd325
pomelo-nwu <czynwu@outlook.com>
2022-05-27 17:22:54 +0800

    feat: 新增edge tooltip 因为体验问题，暂不放开

---
+++ packages/gi-assets-basic/src/components/TooltipForEdge/info.ts
@@ -0,0 +1,10 @@
+const info = {
+  id: 'TooltipForEdge',
+  name: '边提示框',
+  desc: 'Hover 边，展示其详细信息',
+  cover: 'http://xxxx.jpg',
+  category: 'elements-interaction',
+  icon: 'icon-tooltip',
+  type: 'AUTO',
+};
+export default info;
