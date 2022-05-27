4001b7c426736f4fb7bfd56d098af560c83fba1a
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-05-27 17:22:54 +0800

    feat: 新增edge tooltip 因为体验问题，暂不放开

---
+++ packages/gi-assets-basic/src/components/TooltipForEdge/utils.ts
@@ -0,0 +1,14 @@
+export const getValueBySchemaKey = () => {
+  // const LABEL_VALUE = SchemaKey.map((d: string) => {
+  //   const [nodeType, propObjKey, propName] = d.split('.');
+  //   if (node.nodeType === nodeType) {
+  //     // 只有当 nodeType 匹配时才取对应的属性值
+  //     if (propName) {
+  //       // propName 存在，则 propObjKey 值一定为 properties
+  //       return data[propObjKey][propName];
+  //     }
+  //     return data[propObjKey];
+  //   }
+  //   return data[nodeType];
+  // })
+};
