04ca76a6f4ced7611de3fcece316a7b4f9af0fe5
pomelo-nwu <czynwu@outlook.com>
2022-05-31 10:34:07 +0800

    feat: 修改添加sheetbar

--- packages/gi-assets-advance/src/components/AddSheetbar/Component.tsx
+++ packages/gi-assets-advance/src/components/AddSheetbar/Component.tsx
@@ -9,58 +9,18 @@ export interface AddSheetbarProps {
 const AddSheetbar: React.FunctionComponent<AddSheetbarProps> = props => {
   const { handleAddSheetbar, graph } = useContext();
 
-  const [state, setState] = React.useState(() => {
-    return {
-      selected: {
-        nodes: [],
-        edges: [],
-      },
-    };
-  });
-
-  const { selected } = state;
-
   const { GIAC } = props;
   const handleClick = React.useCallback(() => {
-    if (selected.nodes.length === 0) {
+    const nodes = graph.findAllByState('node', 'selected').map(c => c.getModel());
+    const edges = graph.findAllByState('edge', 'selected').map(c => c.getModel());
+    if (nodes.length === 0) {
       return;
     }
-    handleAddSheetbar(selected);
-  }, [selected]);
-
-  React.useEffect(() => {
-    //@ts-ignore
-    graph.on('nodeselectchange', e => {
-      if (e.select) {
-        //单击的时候，edge不存在
-        //@ts-ignore
-        const { nodes, edges } = e.selectedItems;
-        const data = {
-          nodes: nodes.map(n => {
-            return { ...n.getModel() };
-          }),
-          edges: edges
-            ? edges.map(e => {
-                return { ...e.getModel() };
-              })
-            : [],
-        };
-        setState(preState => {
-          return {
-            ...preState,
-            selected: data,
-          };
-        });
-      } else {
-        setState(preState => {
-          return {
-            ...preState,
-            selected: { nodes: [], edges: [] },
-          };
-        });
-      }
+    handleAddSheetbar({
+      nodes,
+      edges,
     });
-  }, []);
+  }, [graph, handleAddSheetbar]);
 
   return (
     <>
