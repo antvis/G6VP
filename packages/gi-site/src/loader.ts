c1ca887527ae0998db8da25666598589b36b3dbb
pomelo-nwu <czynwu@outlook.com>
2022-06-01 22:03:50 +0800

    Thanks to everyone who has contributed code to GraphInsight

--- packages/gi-site/src/loader.ts
+++ packages/gi-site/src/loader.ts
@@ -7,7 +7,7 @@ import { isDev, OFFICIAL_PACKAGES } from '../.umirc';
 setTimeout(() => {
   window.console.clear();
   console.log(
-    '%c Thanks to pomelo.lcw zhanning.bzn xuying.xu yunyi jingxi.lp Yanyan-Wang xx361452 baihui yifeng yuqi.pyq yuran.lcl qingyu hexiaonan.hxn who shared code for GraphInsight',
+    '%c Thanks to pomelo.lcw zhanning.bzn xuying.xu yunyi jingxi.lp Yanyan-Wang xx361452 baihui yifeng yuqi.pyq yuran.lcl qingyu hexiaonan.hxn who have contributed code to GraphInsight',
     'color: #ddd; font-size: 10px; font-style: italic;',
   );
 }, 2000);
