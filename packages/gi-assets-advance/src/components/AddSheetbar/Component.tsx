69f768a72bf4cf7bd2ea32bf19752c7ddee9b655
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-06-01 17:58:32 +0800

    feat: 增加addsheet 的relayout功能

--- packages/gi-assets-advance/src/components/AddSheetbar/Component.tsx
+++ packages/gi-assets-advance/src/components/AddSheetbar/Component.tsx
@@ -1,26 +1,41 @@
 import { extra, IGIAC, useContext } from '@alipay/graphinsight';
+import { message } from 'antd';
 import * as React from 'react';
 
 const { GIAComponent } = extra;
 export interface AddSheetbarProps {
   GIAC: IGIAC;
+  isRelayout: boolean;
 }
 
 const AddSheetbar: React.FunctionComponent<AddSheetbarProps> = props => {
   const { handleAddSheetbar, graph } = useContext();
 
-  const { GIAC } = props;
+  const { GIAC, isRelayout } = props;
   const handleClick = React.useCallback(() => {
-    const nodes = graph.findAllByState('node', 'selected').map(c => c.getModel());
+    const nodes = graph.findAllByState('node', 'selected').map(c => {
+      const { x, y, ...otherModel } = c.getModel();
+      return isRelayout
+        ? otherModel
+        : {
+            x,
+            y,
+            ...otherModel,
+          };
+    });
     const edges = graph.findAllByState('edge', 'selected').map(c => c.getModel());
     if (nodes.length === 0) {
       return;
     }
-    handleAddSheetbar({
-      nodes,
-      edges,
-    });
-  }, [graph, handleAddSheetbar]);
+    if (!handleAddSheetbar) {
+      message.warn('请在默认画布中使用改功能，暂不支持其他画布的「添加页签」功能，预计7月份支持');
+    }
+    handleAddSheetbar &&
+      handleAddSheetbar({
+        nodes,
+        edges,
+      });
+  }, [graph, handleAddSheetbar, isRelayout]);
 
   return (
     <>
