9271efec5db3a8df935646c2236d7b3657d3295f
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-06-21 17:37:26 +0800

    feat: 修改文案

--- packages/gi-assets-basic/src/components/ForceSimulation/Component.tsx
+++ packages/gi-assets-basic/src/components/ForceSimulation/Component.tsx
@@ -18,51 +18,47 @@ export interface IProps {
 
 const ForceSimulation: React.FunctionComponent<IProps> = props => {
   const GIAC = deepClone(props.GIAC);
-  const { graph, layoutInstance } = useContext();
+  const { graph, layoutInstance, layout } = useContext();
   const [state, setState] = React.useState({
     toggle: false,
-    selected: [],
   });
-  const { toggle, selected } = state;
+  const { toggle } = state;
+
+  const isForce = layout.type === 'graphin-force';
 
   const handleClick = () => {
     const { instance = {} } = layoutInstance || {};
-    const { simulation, type } = instance;
-    const isForce = type === 'graphin-force' && simulation;
+    const { simulation } = instance;
 
-    if (isForce) {
-      setState({
-        ...state,
-        toggle: !toggle,
-      });
+    if (isForce && simulation) {
       if (!toggle) {
         simulation.stop();
       } else {
         simulation.restart([], graph);
       }
+      setState({
+        ...state,
+        toggle: !toggle,
+      });
     }
   };
 
   const { autoPin, dragNodeMass = 10000000000 } = props;
 
   React.useEffect(() => {
-    const { instance = {} } = layoutInstance || {};
-    const { simulation, type } = instance;
-
     const handleNodeDragStart = () => {
+      const { instance = {} } = layoutInstance || {};
+      const { simulation } = instance;
       if (simulation) {
         simulation.stop();
       }
     };
     const handleNodeDragEnd = (e: any) => {
-      if (type !== 'graphin-force') {
+      if (!isForce || !autoPin) {
         return;
       }
-      if (!autoPin) {
-        return;
-      }
-
       if (e.item) {
+        console.log('doing...');
         handlePinNode(e.item, graph, layoutInstance, {
           dragNodeMass,
           x: e.x,
@@ -77,10 +73,15 @@ const ForceSimulation: React.FunctionComponent<IProps> = props => {
       graph.off('node:dragstart', handleNodeDragStart);
       graph.off('node:dragend', handleNodeDragEnd);
     };
-  }, [graph, autoPin]);
+  }, [graph, autoPin, isForce, layoutInstance]);
 
   GIAC.icon = toggle ? 'icon-play-circle' : 'icon-pause';
-  GIAC.tooltip = toggle ? '重启力导布局' : '暂停力导布局';
+  GIAC.disabled = true;
+  GIAC.tooltip = '该功能仅在力导布局下才可使用';
+  if (isForce) {
+    GIAC.disabled = false;
+    GIAC.tooltip = toggle ? '重启力导布局' : '暂停力导布局';
+  }
 
   return <GIAComponent GIAC={GIAC} onClick={handleClick} />;
 };
