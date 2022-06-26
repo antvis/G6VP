93f7af6f13a184a562e4f21ad294dedc81571d99
pomelo-nwu <czynwu@outlook.com>
2022-06-26 11:50:17 +0800

    feat: 更新二维码

--- packages/gi-site/src/components/QRcode/index.tsx
+++ packages/gi-site/src/components/QRcode/index.tsx
@@ -1,19 +1,18 @@
-import React from "react";
-import { Dropdown } from "antd";
-import "./index.less"
+import { Dropdown } from 'antd';
+import './index.less';
 const QRcode = () => {
-    const img = (
-        <img
-            style={{ width: "200px",  }}
-            alt="QRcode"
-            src="https://gw.alipayobjects.com/mdn/rms_0d75e8/afts/img/A*4DPGTaxGDRIAAAAAAAAAAAAAARQnAQ"
-        />
-    )
-    return (
-        <Dropdown overlay={img} placement="bottom">
-            <span className="text">加入社区群</span>
-        </Dropdown>
-    )
-}
+  const img = (
+    <img
+      style={{ width: '200px' }}
+      alt="QRcode"
+      src="https://gw.alipayobjects.com/mdn/rms_0d75e8/afts/img/A*-UliQ4r_UnkAAAAAAAAAAAAAARQnAQ"
+    />
+  );
+  return (
+    <Dropdown overlay={img} placement="bottom">
+      <span className="text">加入社区群</span>
+    </Dropdown>
+  );
+};
 
 export default QRcode;
