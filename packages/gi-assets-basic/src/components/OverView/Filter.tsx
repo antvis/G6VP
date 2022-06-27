a9ee6dcaaefac412e61f1d3fd0433e638f2d8715
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-06-28 07:55:51 +0800

    feat: 新增大图模式的overview

---
+++ packages/gi-assets-basic/src/components/OverView/Filter.tsx
@@ -0,0 +1,131 @@
+import { useContext, utils } from '@alipay/graphinsight';
+import { PlusOutlined } from '@ant-design/icons';
+import { GraphinData } from '@antv/graphin';
+import { Button } from 'antd';
+import { nanoid } from 'nanoid';
+import React, { useEffect, useMemo, useState } from 'react';
+import FilterSelection from '../FilterPanel/FilterSelection';
+import '../FilterPanel/index.less';
+import { IFilterCriteria } from '../FilterPanel/type';
+import { filterGraphData } from '../FilterPanel/utils';
+
+const { generatorSchemaByGraphData, isStyles } = utils;
+
+export interface FilterPanelProps {
+  histogramColor: string;
+  isFilterIsolatedNodes: boolean;
+  highlightMode?: boolean;
+  limit: number;
+}
+
+const FilterPanel: React.FunctionComponent<FilterPanelProps> = props => {
+  const { histogramColor, isFilterIsolatedNodes, highlightMode, limit } = props;
+  const [filterOptions, setFilterOptions] = useState<{ [id: string]: IFilterCriteria }>({});
+  const { source, updateContext, transform, schemaData, graph } = useContext();
+  const dataSchemas = schemaData; //useMemo(() => generatorSchemaByGraphData(source), [source]);
+  console.log('dataSchemas', dataSchemas, schemaData, source, limit);
+
+  const nodeProperties = useMemo(() => {
+    return dataSchemas.nodes.reduce((acc, cur) => {
+      return {
+        ...acc,
+        ...cur.properties,
+      };
+    }, {});
+  }, [dataSchemas]);
+
+  const edgeProperties = useMemo(() => {
+    return dataSchemas.edges.reduce((acc, cur) => {
+      return {
+        ...acc,
+        ...cur.properties,
+      };
+    }, {});
+  }, [dataSchemas]);
+
+  const addFilter = () => {
+    const id = nanoid();
+    const filterCriteria = {
+      id,
+      isFilterReady: false,
+    };
+
+    setFilterOptions(preState => {
+      return {
+        ...preState,
+        [id]: filterCriteria,
+      };
+    });
+  };
+
+  const updateFilterCriteria = (id: string, filterCriteria: IFilterCriteria) => {
+    setFilterOptions(preState => {
+      return {
+        ...preState,
+        [id]: filterCriteria,
+      };
+    });
+  };
+
+  const removeFilterCriteria = (id: string) => {
+    delete filterOptions[id];
+    setFilterOptions({ ...filterOptions });
+  };
+
+  useEffect(() => {
+    let data: GraphinData = source;
+
+    Object.values(filterOptions).map(filterCriteria => {
+      data = filterGraphData(data, filterCriteria, isFilterIsolatedNodes);
+    });
+
+    if (highlightMode) {
+      console.log('data', data);
+      // highlightSubGraph(graph, data);
+      if (data.nodes.length < limit) {
+        updateContext(draft => {
+          if (isStyles(source.nodes)) {
+            draft.data = data;
+          } else {
+            draft.data = transform(data);
+          }
+          draft.layoutCache = true;
+        });
+      }
+      return;
+    }
+
+    updateContext(draft => {
+      if (isStyles(source.nodes)) {
+        draft.data = data;
+      } else {
+        draft.data = transform(data);
+      }
+      draft.layoutCache = true;
+    });
+  }, [filterOptions]);
+
+  return (
+    <div className="gi-filter-panel">
+      <Button type="primary" style={{ width: '100%', borderRadius: '4px' }} onClick={addFilter} icon={<PlusOutlined />}>
+        增加筛选器
+      </Button>
+      <div className="gi-filter-panel-criteria-container">
+        {Object.values(filterOptions).map(filterCriter => {
+          return (
+            <FilterSelection
+              filterCriter={filterCriter}
+              nodeProperties={nodeProperties}
+              edgeProperties={edgeProperties}
+              updateFilterCriteria={updateFilterCriteria}
+              removeFilterCriteria={removeFilterCriteria}
+              histogramColor={histogramColor}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FilterPanel;
---
+++ packages/gi-assets-basic/src/components/OverView/registerMeta.ts
@@ -0,0 +1,20 @@
+import { extra } from '@alipay/graphinsight';
+import info from './info';
+const { deepClone, GIAC_CONTENT_METAS } = extra;
+const metas = deepClone(GIAC_CONTENT_METAS);
+metas.GIAC_CONTENT.properties.GIAC_CONTENT.properties.title.default = info.name;
+metas.GIAC_CONTENT.properties.GIAC_CONTENT.properties.icon.default = info.icon;
+metas.GIAC_CONTENT.properties.GIAC_CONTENT.properties.containerWidth.default = '400px';
+
+export default () => {
+  return {
+    limit: {
+      title: '限制展示数量（节点）',
+      type: 'string',
+      'x-decorator': 'FormItem',
+      'x-component': 'NumberPicker',
+      default: 200,
+    },
+    ...metas,
+  };
+};
