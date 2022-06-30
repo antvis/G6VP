9207fd5dfe7750d3219eeb04c383cb52d4d491f4
promise6512 <2523863783@qq.com>
2022-06-30 17:57:18 +0800

    feat:新增 notification 组件

---
+++ packages/gi-site/src/components/Notification/index.tsx
@@ -0,0 +1,75 @@
+import { BellOutlined } from "@ant-design/icons";
+import { IVersionObj } from "./type";
+import { Popover, Button } from "antd";
+import { useEffect } from "react";
+import { useImmer } from "use-immer";
+import { Drawer } from "antd";
+import VersionList from "./VersionList";
+
+export interface IState {
+    popoverVisible: boolean;
+    drawerVisible: boolean;
+    versionInfo: IVersionObj[]
+}
+
+const Notification = () => {
+  const [state, updateState] = useImmer<IState>({
+    popoverVisible: false,
+    drawerVisible: false,
+    versionInfo: [],
+  });
+
+  useEffect(() => {
+    fetch("https://render.alipay.com/p/yuyan/notifcation_version2/zh_CN.json")
+      .then((res) => res.json())
+      .then((versionInfo: IVersionObj[]) => {
+        if (!versionInfo) return;
+        versionInfo.reverse();
+        const { version } = versionInfo[0] || {};
+
+        updateState((draft) => {
+          draft.versionInfo = versionInfo;
+          draft.popoverVisible =
+            version === localStorage.getItem("GI-VERSION") ? false : true;
+        });
+        //localStorage.setItem("GI-VERSION", version);
+      });
+  }, []);
+
+  const content = (
+    <div>
+      <p>GI 新版本上线了!</p>
+    </div>
+  );
+
+  const handleClick = () => {
+    updateState(draft => {
+        draft.drawerVisible = true;
+        draft.popoverVisible = false;
+    })
+  }
+
+  const onCloseDrawer = () => {
+    updateState(draft => {
+        draft.drawerVisible = false;
+    })
+  }
+
+  return (
+    <>
+      <Popover
+        visible={state.popoverVisible}
+        title="版本通知"
+        content={content}
+        arrowPointAtCenter
+      >
+        <Button icon={<BellOutlined />} type="text" onClick={handleClick}></Button>
+      </Popover>
+      <Drawer visible={state.drawerVisible} onClose={onCloseDrawer}>
+        <VersionList versionInfo={state.versionInfo}></VersionList>
+      </Drawer>
+    </>
+  );
+};
+
+export default Notification;
