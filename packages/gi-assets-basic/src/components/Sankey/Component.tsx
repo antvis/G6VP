b7ce4c437309b49e2df1eb4cb81c7f5e111ba7dc
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-07-04 15:33:21 +0800

    feat: 新增桑基图

---
+++ packages/gi-assets-basic/src/components/Sankey/Component.tsx
@@ -0,0 +1,49 @@
+import { useContext } from '@alipay/graphinsight';
+import { Sankey } from '@antv/g2plot';
+import * as React from 'react';
+
+export interface IProps {
+  weightField: string;
+}
+
+const SankeyAnalysis: React.FunctionComponent<IProps> = props => {
+  const { weightField } = props;
+  const { data } = useContext();
+
+  const containerRef = React.useRef<HTMLDivElement | null>(null);
+
+  React.useEffect(() => {
+    const DATA = data.edges
+      .map(e => {
+        return e.data;
+      })
+      .filter(e => {
+        return typeof e[weightField] === 'number';
+      });
+    if (!containerRef.current) {
+      return;
+    }
+
+    const sankey = new Sankey(containerRef.current, {
+      data: DATA,
+      sourceField: 'source',
+      targetField: 'target',
+      weightField: weightField,
+      nodeWidthRatio: 0.01,
+      nodePaddingRatio: 0.03,
+      nodeSort: (a, b) => b[weightField] - a[weightField],
+    });
+    sankey.render();
+    return () => {
+      sankey.destroy();
+    };
+  }, [weightField, data]);
+
+  return (
+    <div>
+      <div ref={containerRef}></div>
+    </div>
+  );
+};
+
+export default SankeyAnalysis;
