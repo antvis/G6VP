3bf035b58acc036f09d85b866ee051a9ff6effde
pomelo-nwu <czynwu@outlook.com>
2022-07-04 15:33:21 +0800

    feat: 新增桑基图

---
+++ packages/gi-assets-basic/src/components/Sankey/info.tsx
@@ -0,0 +1,10 @@
+const info = {
+  id: 'SankeyAnalysis',
+  name: '桑基图分析',
+  desc: '桑基图分析',
+  icon: 'icon-sankey',
+  cover: 'http://xxxx.jpg',
+  category: 'data-analysis',
+  type: 'GIAC_CONTENT',
+};
+export default info;
