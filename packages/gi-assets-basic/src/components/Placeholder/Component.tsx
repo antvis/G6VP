257d141220001c431ae33d375493b98a53d7f680
pomelo-nwu <czynwu@outlook.com>
2022-07-05 15:38:28 +0800

    feat: 增加占位符处理

--- packages/gi-assets-basic/src/components/Placeholder/Component.tsx
+++ packages/gi-assets-basic/src/components/Placeholder/Component.tsx
@@ -8,7 +8,7 @@ export interface LoadingProps {
   width: number;
 }
 
-const Loading: React.FunctionComponent<LoadingProps> = props => {
+const Placeholder: React.FunctionComponent<LoadingProps> = props => {
   const context = useContext();
   const {
     width = 200,
@@ -16,8 +16,17 @@ const Loading: React.FunctionComponent<LoadingProps> = props => {
     text,
   } = props;
 
-  const { data } = context;
+  const { data, largeGraphData, largeGraphLimit } = context;
   const hasNodes = data && data.nodes && data.nodes.length !== 0;
+  if (largeGraphData) {
+    return (
+      <div className="gi-placeholader" style={{ width: `${width}px` }}>
+        {img && <img src={img} width={width} />}
+        当前载入图的节点规模已经超出「{largeGraphLimit}
+        」限制，建议您在资产中心中加载「大图概览」资产，筛选子图数据 或 调整限制规模
+      </div>
+    );
+  }
 
   if (!hasNodes) {
     return (
@@ -27,7 +36,8 @@ const Loading: React.FunctionComponent<LoadingProps> = props => {
       </div>
     );
   }
+
   return null;
 };
 
-export default Loading;
+export default Placeholder;
