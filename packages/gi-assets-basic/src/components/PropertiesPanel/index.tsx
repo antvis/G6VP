910f91cbb5aaa584fdec431392a7c4e8a5d16192
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-07-21 14:22:29 +0800

    feat: remove MockServices

--- packages/gi-assets-basic/src/components/PropertiesPanel/index.tsx
+++ packages/gi-assets-basic/src/components/PropertiesPanel/index.tsx
@@ -1,11 +1,9 @@
 import Component from './Component';
 import info from './info';
-import mockServices from './MockService';
 import registerMeta from './registerMeta';
 
 export default {
   info,
   component: Component,
   registerMeta,
-  mockServices,
 };
