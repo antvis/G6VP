eb3c26825c7a4d72ffedb084c3d9732c68b15c34
pomelo-nwu <czynwu@outlook.com>
2022-07-21 14:22:29 +0800

    feat: remove MockServices

--- packages/gi-assets-advance/src/components/ThemeSetting/info.ts
+++ packages/gi-assets-advance/src/components/ThemeSetting/info.ts
@@ -1,12 +1,12 @@
 const info = {
-    id: 'ThemeSetting',
-    name: '主题设置',
-    desc: '可以自主配置主题样式',
-    icon: 'icon-theme',
-    cover: 'http://xxxx.jpg',
-    category: 'system-interaction',
-    type: 'GIAC_CONTENT',
-  };
-  
-  export default info;
-  
+  id: 'ThemeSetting',
+  name: '主题设置',
+  desc: '可以自主配置主题样式',
+  icon: 'icon-theme',
+  cover: 'http://xxxx.jpg',
+  category: 'system-interaction',
+  type: 'GIAC_CONTENT',
+  services: ['AddTheme', 'RemoveTheme', 'GetTheme', 'UpdateTheme'],
+};
+
+export default info;
--- packages/gi-assets-basic/src/components/NeighborsQuery/index.tsx
+++ packages/gi-assets-basic/src/components/NeighborsQuery/index.tsx
@@ -1,11 +1,10 @@
 import Component from './Component';
 import info from './info';
-import mockServices from './mockServices';
+
 import registerMeta from './registerMeta';
 
 export default {
   info,
   component: Component,
   registerMeta,
-  mockServices,
 };
