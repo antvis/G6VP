b6b55c078879752f195fee954b3e8b1bacb8ce47
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-07-25 11:19:45 +0800

    feat: 追加本地gi-assets-shaseng

--- packages/gi-site/src/loader.ts
+++ packages/gi-site/src/loader.ts
@@ -2,8 +2,11 @@ import * as GI_ASSETS_ADVANCE from '@alipay/gi-assets-advance';
 import * as GI_ASSETS_ALGORITHM from '@alipay/gi-assets-algorithm';
 // import * as GI_ASSETS_ANALYSIS from '@alipay/gi-assets-analysis';
 import * as GI_ASSETS_BASIC from '@alipay/gi-assets-basic';
-import * as GI_ASSETS_GS from '@alipay/gi-assets-gs';
 import * as GI_ASSETS_SCENE from '@alipay/gi-assets-scene';
+/** 外部的引擎包，软连接到这里，临时方案，后续删除 */
+import * as GI_ASSETS_GS from '@alipay/gi-assets-gs';
+import * as GI_ASSETS_SHASENG from '@alipay/gi-assets-shaseng';
+
 import { getPackages, isDev, OFFICIAL_PACKAGES } from '../.umirc';
 
 // 临时方案，应该要走antbuc鉴权
@@ -69,6 +72,12 @@ const LOCAL_ASSETS = [
     version: '1.0.0',
     global: 'GI_ASSETS_GS',
     ...GI_ASSETS_GS,
+  },
+  {
+    name: '@alipay/gi-assets-shaseng',
+    version: '1.0.0',
+    global: 'GI_ASSETS_SHASENG',
+    ...GI_ASSETS_SHASENG,
   },
   // {
   //   ...OFFICIAL_PACKAGES_MAP['GI_ASSETS_ANALYSIS'],
