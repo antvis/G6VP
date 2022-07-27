412b12621e20ec5327f234ca1f4e6594dc95c4f3
promise6512 <2523863783@qq.com>
2022-07-27 19:14:08 +0800

    style: ts 补充

--- packages/gi-assets-basic/src/services/ServerComponent/service.ts
+++ packages/gi-assets-basic/src/services/ServerComponent/service.ts
@@ -1,5 +1,12 @@
 import localforage from 'localforage';
 import { message } from 'antd';
+import { utils } from '@alipay/graphinsight';
+import { GraphinData } from '@antv/graphin';
+import { notification } from 'antd';
+import { IInputData } from './type';
+
+const { generatorSchemaByGraphData, generatorStyleConfigBySchema } = utils;
+
 export const getProjectById = async (id: string) => {
   const project: any = await localforage.getItem(id);
   if (!project) {
@@ -54,8 +61,67 @@ export const updateProjectById = async (id: string, params: { data?: string; [ke
     origin.expandInfo = JSON.parse(expandInfo);
   }
 
-  /* if (type) {
-        origin.type = type;
-      } */
   return await localforage.setItem(id, origin);
+};
+
+export const updateData = async (transData: GraphinData, inputData: IInputData[], transfunc: string) => {
+  try {
+    if (transData.nodes?.find(d => d.id === undefined || d.data === undefined)) {
+      throw 'nodes缺少对应字段';
+    }
+    if (transData.edges?.find(d => d.source === undefined || d.target === undefined || d.data === undefined)) {
+      throw 'edges缺少对应字段';
+    }
+
+    // 对于GI平台，是这样取得projectId的
+    const hash = window.location.hash;
+    const id = hash.split('/')[2].split('?')[0];
+
+    const result = await getProjectById(id);
+
+    const beforData = result.data.transData;
+    const mergeData = {
+      nodes: [...beforData.nodes, ...transData.nodes],
+      edges: [...beforData.edges, ...transData.edges],
+    };
+
+    // 进入分析之前，根据数据，生成 schema
+
+    const schemaData = generatorSchemaByGraphData(mergeData);
+    const newConfig = generatorStyleConfigBySchema(schemaData, result.config);
+
+    // 更新inputdata里面的 trans function
+    const renderData = inputData.map(d => {
+      return {
+        ...d,
+        transfunc,
+      };
+    });
+
+    console.log('renderData:', renderData);
+
+    updateProjectById(id, {
+      data: JSON.stringify({
+        transData: mergeData,
+        inputData: [...result.data.inputData, ...renderData],
+      }),
+      // schemaData: schemaData,
+      projectConfig: JSON.stringify(newConfig),
+      schemaData: JSON.stringify(schemaData),
+    }).then(res => {
+      location.reload();
+    });
+
+    notification.success({
+      message: `解析成功`,
+      description: `数据格式正确`,
+      placement: 'topLeft',
+    });
+  } catch (error) {
+    notification.error({
+      message: `解析出错`,
+      description: `请检查数据是否为严格JSON格式且存在对应字段:${error}`,
+      placement: 'topLeft',
+    });
+  }
 };
