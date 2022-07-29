c32f7d81ce78f210f485ce844de6b457e31a0d7f
promise6512 <2523863783@qq.com>
2022-07-29 20:22:30 +0800

    feat：补充 ts

--- packages/gi-site/src/pages/Analysis/useModel.ts
+++ packages/gi-site/src/pages/Analysis/useModel.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { GIAssets, GIConfig, GIService } from '@alipay/graphinsight/src/typing';
-import { GraphinData } from '@antv/graphin';
 import { useImmer, Updater } from 'use-immer';
+import { StateType } from "./typing"
 
 export const initialState: StateType = {
   /** 项目ID */
@@ -59,61 +58,7 @@ export const initialState: StateType = {
   },
 };
 
-export interface StateType {
-  /** 项目ID */
-  id: string;
-  /** GISDK.config */
-  config: GIConfig;
-  /** GISDK.services */
-  services: GIService[];
-  /** 数据服务列表 */
-  serviceConfig: {
-    id: string;
-    mode: string;
-    content: string;
-  }[];
-  /** 强制渲染的React Key */
-  key: number;
-  /** 原始数据 */
-  data: GraphinData;
-  /** 上传的数据 */
-  inputData: any[];
-  transfunc: string;
-  /** 全部资产 */
-  assets: GIAssets;
-  /** 用户选择的资产,活跃资产 */
-  activeAssets: GIAssets | null;
-  /** 用户选择的资产的Key值,活跃资产 */
-  activeAssetsKeys: {
-    components: string[];
-    elements: string[];
-    layouts: string[];
-  } | null;
-  /** 激活资产的详细信息：meta/info */
-  activeAssetsInformation: {
-    components: any[];
-    elements: { nodes: {}; edges: {} };
-    layouts: any;
-  } | null;
-  /** 是否准备完毕 */
-  isReady: boolean;
-  /** 是否保存 */
-  isSave: boolean;
-  /** 当前 Sidebar 的值 */
-  activeNavbar: string;
-  /** 当前 Sidebar 是否可折叠 */
-  collapse: boolean;
 
-  /** 是否开启智能推荐 */
-  enableAI: boolean;
-  /** 原始渲染的配置，用于取消智能推荐时还原 */
-  projectConfig: GIConfig;
-  assetsCenter: {
-    visible: boolean;
-    hash: string;
-  };
-  schemaData: any;
-}
 
 const useModel: () => [StateType, Updater<StateType>] = () => {
   const [state, updateState] = useImmer<StateType>(initialState);
