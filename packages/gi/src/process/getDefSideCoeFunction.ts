4b2cef4e2ecaa199248e10e49e06ab4a56e39577
yunyi <2523863783@qq.com>
2022-08-01 14:49:05 +0800

    style: 删除 console

--- packages/gi/src/process/getDefSideCoeFunction.ts
+++ packages/gi/src/process/getDefSideCoeFunction.ts
@@ -1,4 +1,4 @@
-export const getDefSideCoe = (income: string, outcome: string, isLog: string, multiple: string) => {
+export const getDefSideCoeFunction = (income: string, outcome: string, isLog: string, multiple: string) => {
   const defSideCoe = (node): number => {
     let value = 0;
     try {
@@ -7,12 +7,10 @@ export const getDefSideCoe = (income: string, outcome: string, isLog: string, mu
       }
       const { data } = node.data;
       value = data[income] - data[outcome];
-      //console.log("value before:", node.data,income,node.data[income] , node.data[outcome],node.data[income] -node.data[outcome])
       if (isLog) {
         value = (value / Math.abs(value)) * Math.log(Math.abs(value));
       }
       value = value * parseFloat(multiple);
-      //console.log('value',value)
       return value;
     } catch (e) {
         console.log(e)
