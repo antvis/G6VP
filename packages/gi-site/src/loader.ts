9fae6de4f4c23649c339875d8afc44773024cafb
pomelo-nwu <czynwu@outlook.com>
2022-08-03 17:02:10 +0800

    feat: 更改配置

--- packages/gi-site/src/loader.ts
+++ packages/gi-site/src/loader.ts
@@ -4,9 +4,9 @@ import * as GI_ASSETS_ALGORITHM from '@alipay/gi-assets-algorithm';
 import * as GI_ASSETS_BASIC from '@alipay/gi-assets-basic';
 import * as GI_ASSETS_SCENE from '@alipay/gi-assets-scene';
 /** 外部的引擎包，软连接到这里，临时方案，后续删除 */
-import * as GI_ASSETS_GS_LOCAL from '@alipay/gi-asset-gs-local';
-import * as GI_ASSETS_AKG from '@alipay/gi-assets-akg';
-import * as GI_ASSETS_GS from '@alipay/gi-assets-gs';
+// import * as GI_ASSETS_GS_LOCAL from '@alipay/gi-asset-gs-local';
+// import * as GI_ASSETS_AKG from '@alipay/gi-assets-akg';
+// import * as GI_ASSETS_GS from '@alipay/gi-assets-gs';
 // import * as GI_ASSETS_SHASENG from '@alipay/gi-assets-shaseng';
 import * as GI_SERVER_LOCAL from '@alipay/gi-server-local';
 
@@ -39,13 +39,13 @@ export const BIZ_PACKAGES = IS_PASSED_BUC_AUTH
     ])
   : [];
 
-setTimeout(() => {
-  // !isDev && window.console.clear();
-  console.log(
-    '%c Thanks to pomelo.lcw zhanning.bzn xuying.xu yunyi jingxi.lp Yanyan-Wang axu.zx xx361452 baihui yifeng yuqi.pyq yuran.lcl qingyu hexiaonan.hxn who have contributed code to GraphInsight',
-    'color: #ddd; font-size: 10px; font-style: italic;',
-  );
-}, 3000);
+// setTimeout(() => {
+//   // !isDev && window.console.clear();
+//   console.log(
+//     '%c Thanks to pomelo.lcw zhanning.bzn xuying.xu yunyi jingxi.lp Yanyan-Wang axu.zx xx361452 baihui yifeng yuqi.pyq yuran.lcl qingyu hexiaonan.hxn who have contributed code to GraphInsight',
+//     'color: #ddd; font-size: 10px; font-style: italic;',
+//   );
+// }, 3000);
 const OFFICIAL_PACKAGES_MAP = OFFICIAL_PACKAGES.reduce((acc, curr) => {
   return {
     ...acc,
@@ -53,6 +53,12 @@ const OFFICIAL_PACKAGES_MAP = OFFICIAL_PACKAGES.reduce((acc, curr) => {
   };
 }, {});
 const LOCAL_ASSETS = [
+  /** 内置的引擎 */
+  {
+    ...OFFICIAL_PACKAGES_MAP['GI_SERVER_LOCAL'],
+    ...GI_SERVER_LOCAL,
+  },
+  /** 内置的资产 */
   {
     ...OFFICIAL_PACKAGES_MAP['GI_ASSETS_BASIC'],
     ...GI_ASSETS_BASIC,
@@ -69,39 +75,34 @@ const LOCAL_ASSETS = [
     ...OFFICIAL_PACKAGES_MAP['GI_ASSETS_SCENE'],
     ...GI_ASSETS_SCENE,
   },
-  {
-    name: '@alipay/gi-assets-gs',
-    version: '1.0.0',
-    global: 'GI_ASSETS_GS',
-    ...GI_ASSETS_GS,
-  },
-  {
-    name: '@alipay/gi-assets-akg',
-    version: '1.0.0',
-    global: 'GI_ASSETS_AKG',
-    ...GI_ASSETS_AKG,
-  },
-  {
-    name: '@alipay/gi-server-local',
-    version: '1.0.0',
-    global: 'GI_SERVER_LOCAL',
-    ...GI_SERVER_LOCAL,
-  },
-  {
-    name: '@alipay/gi-asset-gs-local',
-    version: '1.0.0',
-    global: 'GI_ASSETS_GS_LOCAL',
-    ...GI_ASSETS_GS_LOCAL,
-  },
+  // {
+  //   ...OFFICIAL_PACKAGES_MAP['GI_ASSETS_ANALYSIS'],
+  //   ...GI_ASSETS_ANALYSIS,
+  // },
+  /** 第三方资产库 **/
+  // {
+  //   name: '@alipay/gi-assets-gs',
+  //   version: '1.0.0',
+  //   global: 'GI_ASSETS_GS',
+  //   ...GI_ASSETS_GS,
+  // },
+  // {
+  //   name: '@alipay/gi-assets-akg',
+  //   version: '1.0.0',
+  //   global: 'GI_ASSETS_AKG',
+  //   ...GI_ASSETS_AKG,
+  // },
+  // {
+  //   name: '@alipay/gi-asset-gs-local',
+  //   version: '1.0.0',
+  //   global: 'GI_ASSETS_GS_LOCAL',
+  //   ...GI_ASSETS_GS_LOCAL,
+  // },
   // {
   //   name: '@alipay/gi-assets-shaseng',
   //   version: '1.0.0',
   //   global: 'GI_ASSETS_SHASENG',
   //   ...GI_ASSETS_SHASENG,
-  // },
-  // {
-  //   ...OFFICIAL_PACKAGES_MAP['GI_ASSETS_ANALYSIS'],
-  //   ...GI_ASSETS_ANALYSIS,
   // },
 ];
 
