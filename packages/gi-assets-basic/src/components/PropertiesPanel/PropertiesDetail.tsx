f56f1dcf7699e3ec163bf901f966dcc9b90e2ef2
yunyi <2523863783@qq.com>
2022-08-04 12:55:15 +0800

    fix: 支持搜索属性

--- packages/gi-assets-basic/src/components/PropertiesPanel/PropertiesDetail.tsx
+++ packages/gi-assets-basic/src/components/PropertiesPanel/PropertiesDetail.tsx
@@ -1,36 +1,79 @@
-import React from "react";
-import { Input } from "antd";
+import React from 'react';
+import { Input, Empty } from 'antd';
+import { useImmer } from 'use-immer';
 const Search = Input.Search;
 
 interface PropertiesDetailProp {
   data: any;
 }
 
-const PropertiesDetail: React.FC<PropertiesDetailProp> = (props) => {
-  const { data } = props;
+interface PropertiesDetailState {
+  isSearch: boolean;
+  searchKey: string;
+}
 
-  const onSearch = () => {};
+interface IItem {
+  key: string;
+  value: string;
+  isObject: boolean;
+}
+
+const PropertiesDetail: React.FC<PropertiesDetailProp> = props => {
+  const { data } = props;
+  const [state, updateState] = useImmer<PropertiesDetailState>({
+    isSearch: false,
+    searchKey: '',
+  });
+
+  const onSearch = (value: string) => {
+    const isSearch = value.length !== 0;
+    updateState(draft => {
+      draft.isSearch = isSearch;
+      draft.searchKey = value;
+    });
+  };
+
+  let content: IItem[] = Object.keys(data).map(key => {
+    let value = data[key];
+    let isObject = false;
+    if (typeof value == 'object') {
+      value = JSON.stringify(value, null, 2);
+      isObject = true;
+    }
+    if (typeof value !== 'string') {
+      value = String(value);
+    }
+    return {
+      key,
+      value,
+      isObject,
+    };
+  });
+
+  if (state.isSearch) {
+    content = content.filter((item: IItem) => {
+      const { key, value } = item;
+      return key.includes(state.searchKey) || value.includes(state.searchKey);
+    });
+  }
+
   return (
     <>
-      <Search
-        placeholder="Search in the properties"
-        onSearch={onSearch}
-        style={{ width: "100%" }}
-      />
+      <Search placeholder="Search in the properties" onSearch={onSearch} style={{ width: '100%' }} />
       <ul>
-        {Object.keys(data).map((key) => {
-          let content = data[key];
-          let isObject = false;
-          if (typeof content == "object") {
-            content = JSON.stringify(content, null, 2);
-            isObject = true;
-          }
+        {content.length === 0 && (
+          <Empty
+            style={{
+              marginTop: '50px',
+            }}
+          />
+        )}
+        {content.map(item => {
+          const { key, value, isObject } = item;
           return (
             <li key={key}>
               <div className="key">{key}</div>
-              <div className="value">
-                {isObject ? <pre>{content}</pre> : content}
-              </div>
+              <div className="value">{isObject ? <pre>{value}</pre> : value}</div>
             </li>
           );
         })}
