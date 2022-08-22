462e9d1bd00541b2f53414652831d791975ddfae
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-08-22 13:39:24 +0800

    fix custom services

--- packages/gi-site/src/pages/Analysis/getAssets/getServicesByConfig.ts
+++ packages/gi-site/src/pages/Analysis/getAssets/getServicesByConfig.ts
@@ -12,14 +12,29 @@ const getServicesByConfig = (serviceConfig, LOCAL_DATA, schemaData) => {
   return serviceConfig.map(s => {
     const { id, content, mode } = s;
     const runtimeContent = content?.split('export default')[1] || content;
-    const transFn = looseJsonParse(runtimeContent);
-    return {
-      id,
-      content,
-      service: (...params) => {
-        return transFn(...params, LOCAL_DATA, schemaData);
-      },
-    };
+    try {
+      const transFn = looseJsonParse(runtimeContent);
+      return {
+        id,
+        content,
+        service: (...params) => {
+          return transFn(...params, LOCAL_DATA, schemaData);
+        },
+      };
+    } catch (error) {
+      return {
+        id,
+        content,
+        service: () => {
+          return new Promise(resolve => {
+            resolve({
+              nodes: [],
+              edges: [],
+            });
+          });
+        },
+      };
+    }
   });
 };
 
