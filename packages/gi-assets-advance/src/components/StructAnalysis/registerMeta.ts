547aba0dfdec3c28ebf4507daeb688c99a9c4db8
yunyi <2523863783@qq.com>
2022-08-24 15:21:28 +0800

    feat: struct-analysis

---
+++ packages/gi-assets-advance/src/components/StructAnalysis/registerMeta.ts
@@ -0,0 +1,19 @@
+import { extra } from '@alipay/graphinsight';
+import info from './info';
+const { deepClone, GIAC_CONTENT_METAS } = extra;
+const metas = deepClone(GIAC_CONTENT_METAS);
+
+metas.GIAC_CONTENT.properties.GIAC_CONTENT.properties.title.default = info.name;
+metas.GIAC_CONTENT.properties.GIAC_CONTENT.properties.icon.default = info.icon;
+metas.GIAC_CONTENT.properties.GIAC_CONTENT.properties.tooltip.default = info.desc;
+metas.GIAC_CONTENT.properties.GIAC_CONTENT.properties.containerWidth.default = '400px';
+
+const registerMeta = () => {
+  const schema = {
+    ...metas,
+  };
+
+  return schema;
+};
+
+export default registerMeta;
