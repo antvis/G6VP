f94315194f6db77727582ece2e011007321786ce
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-08-29 10:59:12 +0800

    feat: 增加Force2资产

---
+++ packages/gi-assets-basic/src/layouts/Force2/index.tsx
@@ -0,0 +1,26 @@
+import registerLayout from './registerLayout';
+import registerMeta from './registerMeta';
+
+/**   index.md 中解析得到默认值，也可用户手动修改 */
+const info = {
+  id: 'Force2',
+  options: {
+    type: 'force2',
+    animation: false,
+    preset: {
+      type: 'concentric',
+    },
+  },
+  name: '渐进力导',
+  category: 'basic',
+  type: 'LAYOUT',
+  desc: '渐进式力导布局，可用于动态布局',
+  icon: 'icon-layout-force',
+  cover: 'http://xxxx.jpg',
+};
+
+export default {
+  info,
+  registerLayout,
+  registerMeta,
+};
--- packages/gi-assets-basic/src/layouts/index.tsx
+++ packages/gi-assets-basic/src/layouts/index.tsx
@@ -1,12 +1,13 @@
 import Circular from './Circular';
 import Concentric from './Concentric';
 // import D3Force from './D3Force';
+import ClusteringDagre from './ClusteringDagre';
 import Dagre from './Dagre';
+import Force2 from './Force2';
+import FundForce from './FundForce';
 import GraphinForce from './GraphinForce';
 import Grid from './Grid';
-import FundForce from './FundForce';
 import Radial from './Radial';
-import ClusteringDagre from './ClusteringDagre';
 // import Random from './Random';
 /** 图布局市场 */
-export { Concentric, Dagre, GraphinForce, Grid, Radial, Circular, ClusteringDagre, FundForce };
+export { Force2, Concentric, Dagre, GraphinForce, Grid, Radial, Circular, ClusteringDagre, FundForce };
