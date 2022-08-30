38a21e4e4ba90b92564b3e4ac1816ec397de5698
promise6512 <2523863783@qq.com>
2022-08-30 17:56:18 +0800

    fix: 状态重置

--- packages/gi-common-components/src/CommonStyleSetting/index.tsx
+++ packages/gi-common-components/src/CommonStyleSetting/index.tsx
@@ -113,10 +113,13 @@ const CommonStyleSetting: React.FunctionComponent<StyleSettingProps> = ({
     const resultGroup: any = [];
     for (const group of all.groups) {
       // 从 preStyleGroup 中过滤出相同 ID 的对象，进行 merge
-      const currentGroup = preStyleGroup.current?.find((pg: any) => pg.groupId === group.groupId);
+      const currentGroup = preStyleGroup.current?.find((pg: any) => pg.groupName === group.groupName);
       if (currentGroup) {
         // 进行 merge
         const result = Object.assign({}, currentGroup, group);
+        result.props = {
+          ...currentGroup.props,
+        };
         resultGroup.push(result);
       } else {
         resultGroup.push(group);
