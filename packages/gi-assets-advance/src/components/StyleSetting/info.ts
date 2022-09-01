086aba18bcb39ba4b2974921d788a236c0e1ab5a
promise6512 <2523863783@qq.com>
2022-09-01 14:13:34 +0800

    feat: add style-setting service

--- packages/gi-assets-advance/src/components/StyleSetting/info.ts
+++ packages/gi-assets-advance/src/components/StyleSetting/info.ts
@@ -1,10 +1,12 @@
+const ASSET_ID = 'StyleSetting'
 const info = {
-  id: 'StyleSetting',
+  id: ASSET_ID,
   name: '样式设置',
   desc: '设置元素样式，可自定义分组规则',
   icon: 'icon-bg-colors',
   cover: 'http://xxxx.jpg',
   category: 'elements-interaction',
+  services: [ASSET_ID],
   type: 'GIAC_CONTENT',
 };
 export default info;
