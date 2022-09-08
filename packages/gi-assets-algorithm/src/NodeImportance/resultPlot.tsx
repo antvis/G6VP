5883e076a27c2bdd28081a06064ca307ba42b2ce
pomelo.lcw <pomelo.lcw@antgroup.com>
2022-09-08 14:48:07 +0800

    feat: 更新部署模块

--- packages/gi-assets-algorithm/src/NodeImportance/resultPlot.tsx
+++ packages/gi-assets-algorithm/src/NodeImportance/resultPlot.tsx
@@ -1,8 +1,8 @@
-import { Column } from "@antv/g2plot";
-import { GraphinContext } from "@antv/graphin";
-import { groupBy } from "lodash";
-import React, { useContext, useEffect, useRef } from "react";
-import { ITEM_STATE } from "./registerMeta";
+import { Column } from '@antv/g2plot';
+import { GraphinContext } from '@antv/graphin';
+import { groupBy } from 'lodash';
+import React, { useContext, useEffect, useRef } from 'react';
+import { ITEM_STATE } from './registerMeta';
 
 interface Props {
   data;
@@ -25,30 +25,30 @@ const ResultTable: React.FC<Props> = ({ data, currentAlgo, edgeType }) => {
     //@ts-ignore
     plot = new Column(plotRef.current, {
       data: plotData,
-      xField: "value",
-      yField: "count",
+      xField: 'value',
+      yField: 'count',
       maxColumnWidth: 60,
-      colorField: "count",
+      colorField: 'count',
       legend: {
-        layout: "horizontal",
-        position: "bottom",
+        layout: 'horizontal',
+        position: 'bottom',
         offsetY: 4,
         maxRow: 2,
       },
       label: {
-        position: "middle",
+        position: 'middle',
         style: {
-          fill: "#FFFFFF",
+          fill: '#FFFFFF',
           opacity: 0.6,
         },
       },
       xAxis: {
         title: {
-          text: "重要性值",
-          position: "end",
+          text: '重要性值',
+          position: 'end',
           offset: 14,
           style: {
-            stroke: "#fff",
+            stroke: '#fff',
             lineWidth: 2,
             fontWeight: 400,
           },
@@ -56,12 +56,12 @@ const ResultTable: React.FC<Props> = ({ data, currentAlgo, edgeType }) => {
       },
       yAxis: {
         title: {
-          text: "节点数量",
-          position: "end",
+          text: '节点数量',
+          position: 'end',
           autoRotate: false,
           spacing: -55,
           style: {
-            stroke: "#fff",
+            stroke: '#fff',
             lineWidth: 2,
             fontWeight: 400,
           },
@@ -69,14 +69,14 @@ const ResultTable: React.FC<Props> = ({ data, currentAlgo, edgeType }) => {
       },
     });
     plot.render();
-    plot.on("interval:mouseleave", clearActiveItems);
-    plot.on("interval:mouseenter", onEnterColumn);
+    plot.on('interval:mouseleave', clearActiveItems);
+    plot.on('interval:mouseenter', onEnterColumn);
   }, [data]);
 
   const getPlotData = () => {
     const plotData = [];
-    const countMap = groupBy(data.node?.data || [], "value");
-    Object.keys(countMap).forEach((key) => {
+    const countMap = groupBy(data.node?.data || [], 'value');
+    Object.keys(countMap).forEach(key => {
       plotData.push({
         //@ts-ignore
         value: key,
@@ -89,23 +89,23 @@ const ResultTable: React.FC<Props> = ({ data, currentAlgo, edgeType }) => {
 
   const clearActiveItems = () => {
     const activateItems = graph
-      .findAllByState("node", ITEM_STATE.Active)
-      .concat(graph.findAllByState("edge", ITEM_STATE.Active));
-    activateItems.forEach((item) => {
+      .findAllByState('node', ITEM_STATE.Active)
+      .concat(graph.findAllByState('edge', ITEM_STATE.Active));
+    activateItems.forEach(item => {
       graph.setItemState(item, ITEM_STATE.Active, false);
     });
   };
 
-  const onEnterColumn = (e) => {
+  const onEnterColumn = e => {
     if (!e.data?.data) {
       return;
     }
     const value = e.data?.data?.value;
     clearActiveItems();
-    const nodes = data.node.data.filter((node) => +node.value === +value);
-    const nodeIds = nodes.map((node) => node.id);
+    const nodes = data.node.data.filter(node => +node.value === +value);
+    const nodeIds = nodes.map(node => node.id);
 
-    nodes.forEach((node) => {
+    nodes.forEach(node => {
       const item = graph.findById(node.id);
       if (item) {
         graph.setItemState(item, ITEM_STATE.Active, true);
@@ -113,17 +113,14 @@ const ResultTable: React.FC<Props> = ({ data, currentAlgo, edgeType }) => {
     });
 
     // 若未关系属性重要性, 同时高亮该节点相关的被映射过的边
-    if (currentAlgo === "edge-property") {
-      data.edge?.data?.forEach((edge) => {
+    if (currentAlgo === 'edge-property') {
+      data.edge?.data?.forEach(edge => {
         const edgeItem = graph.findById(edge.id);
         if (!edgeItem) {
           return;
         }
         const edgeModel = edgeItem.getModel();
-        if (
-          nodeIds.includes(edgeModel.source) ||
-          nodeIds.includes(edgeModel.target)
-        ) {
+        if (nodeIds.includes(edgeModel.source) || nodeIds.includes(edgeModel.target)) {
           graph.setItemState(edgeItem, ITEM_STATE.Active, true);
         }
       });
