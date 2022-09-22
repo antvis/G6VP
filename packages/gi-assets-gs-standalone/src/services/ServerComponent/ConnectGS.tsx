0ce000e097983e54280a3a198360ed222b19aeeb
baizn <576375879@qq.com>
2022-09-22 16:58:31 +0800

    fix: GS 单机版服务配置及组件修改

--- packages/gi-assets-gs-standalone/src/services/ServerComponent/ConnectGS.tsx
+++ packages/gi-assets-gs-standalone/src/services/ServerComponent/ConnectGS.tsx
@@ -1,66 +1,17 @@
 // @ts-nocheck
 import { GISiteParams } from '@alipay/graphinsight';
-import { Alert, Switch, Col, Form, Input, message, Row, Select } from 'antd';
-import React, { useState } from 'react';
+import { Alert, Switch, Col, Form, Input, Row, Select } from 'antd';
+import React from 'react';
 import './index.less';
 
 const { Option } = Select;
+
 export interface GraphModelProps {
   updateGISite?: (params: GISiteParams) => void;
   giSiteContext?: any;
+  loading?: boolean;
 }
-const ConnectGraphScope: React.FC<GraphModelProps> = ({ updateGISite }) => {
-  const [form] = Form.useForm();
-  const [loading, setLoading] = useState(false);
-  const [addonBefore, setAddonBefore] = useState('http://');
-
-  const handleSubmitForm = async () => {
-    setLoading(true);
-    // 使用示例数据
-
-    const values = await form.validateFields();
-
-    const { username, password, serverUrl } = values;
-
-    // 加上传的文件加载仅 GraphScope
-    const url = `${addonBefore}${serverUrl}`;
-    const result = await connectTuGraphDataSource(username, password, url);
-
-    console.log('TuGraph 连接', result);
-    setLoading(false);
-
-    message.success('连接 TuGraph 数据源成功');
-
-    // const p1 = GI_SERVICE_INTIAL_GRAPH.service();
-    // const p2 = GI_SERVICE_SCHEMA.service({ graphName: 'default' });
-    // Promise.all([p1, p2]).then(values => {
-    //   const [_data, schema] = values;
-    //   updateGISite &&
-    //     updateGISite({
-    //       engineId: 'TuGraph',
-    //       schemaData: schema,
-    //       engineContext: {},
-    //       /**
-    //        * GI平台上会localStorage.setItem('SERVER_ENGINE_CONTEXT',JSON.stringify(engineContext))
-    //        * 因此，在接口层，只需要调用 localStorage.getItem('SERVER_ENGINE_CONTEXT') 即可拿到服务上下文
-    //        */
-    //     });
-    // });
-
-    // onClose();
-  };
-
-  const handleChange = value => {
-    setAddonBefore(value);
-  };
-
-  const selectBefore = (
-    <Select defaultValue="http://" className="select-before" onChange={handleChange}>
-      <Option value="http://">http://</Option>
-      <Option value="https://">https://</Option>
-    </Select>
-  );
-
+const ConnectGraphScope: React.FC<GraphModelProps> = ({ loading }) => {
   return (
     <Row style={{ paddingTop: 16 }}>
       <Col span={10} className="logo-container">
@@ -80,14 +31,24 @@ const ConnectGraphScope: React.FC<GraphModelProps> = ({ updateGISite }) => {
           name="httpServerURL"
           rules={[{ required: true, message: '部署GraphScope的服务器地址必填!' }]}
         >
-          <Input addonBefore={selectBefore} placeholder="请输入提供Gremlin查询的HTTP服务地址，格式为 ip:port" />
+          <Select mode="tags" style={{ width: '100%' }}>
+            <Option key="http://127.0.0.1:7001">GI 本地开发环境地址</Option>
+            <Option key="http://storehouse.test.alipay.net">GI 测试环境地址</Option>
+            <Option key="http://graphinsight-pre.alipay.com">GI 预发环境地址</Option>
+            <Option key="http://graphinsight.antgroup-inc.cn">GI 生产环境地址</Option>
+          </Select>
+          {/* <Input placeholder="请输入提供Gremlin查询的HTTP服务地址，格式为 http://ip:port" /> */}
         </Form.Item>
         <Form.Item
           label="引擎服务地址"
           name="engineServerURL"
           rules={[{ required: true, message: '部署GraphScope的服务器地址必填!' }]}
         >
-          <Input addonBefore={selectBefore} placeholder="请输入部署GraphScope的服务器，格式为 ip:port" />
+          <Select mode="tags" style={{ width: '100%' }}>
+            <Option key="http://11.166.85.48:9527">蚂蚁线下环境</Option>
+            <Option key="http://grape.alibaba-inc.com">阿里线上环境</Option>
+          </Select>
+          {/* <Input placeholder="请输入部署GraphScope的服务器，格式为 http://ip:port" /> */}
         </Form.Item>
         <Form.Item label="ID字段类型" name="isStringType">
           <Switch defaultChecked checkedChildren="string" unCheckedChildren="int64" />
