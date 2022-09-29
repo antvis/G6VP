8563a83bbf835397c089e5fd7beb11cccc5bbe72
pomelo-nwu <czynwu@outlook.com>
2022-09-29 16:53:09 +0800

    feat(sdk): 增加聚合模式

--- packages/gi-assets-basic/src/components/Initializer/Component.tsx
+++ packages/gi-assets-basic/src/components/Initializer/Component.tsx
@@ -7,11 +7,12 @@ export type GIService = any;
 export interface IProps {
   serviceId: string;
   schemaServiceId: string;
+  aggregate: boolean;
 }
 
 const Initializer: React.FunctionComponent<IProps> = props => {
   const context = useContext();
-  const { serviceId, schemaServiceId } = props;
+  const { serviceId, schemaServiceId, aggregate } = props;
   const { services, updateContext, transform, largeGraphLimit } = context;
 
   React.useEffect(() => {
@@ -24,52 +25,69 @@ const Initializer: React.FunctionComponent<IProps> = props => {
       updateContext(draft => {
         const { nodes } = data;
 
-        if (schema) {
-          // 更新schemaData
-          draft.schemaData = schema as any;
-          //只有当config中没有nodes和edges的时候，才会用schema生成一个默认样式
-          if (draft.config.nodes?.length === 0 || draft.config.edges?.length === 0) {
-            const schemaStyle = utils.generatorStyleConfigBySchema(schema);
-            draft.config.nodes = schemaStyle.nodes;
-            draft.config.edges = schemaStyle.edges;
-          }
-        }
-
+        /** 判断是否保存样式和位置 */
         const position = isPosition(nodes);
         const style = isStyles(nodes);
+        /** 取消布局缓存 */
+        draft.initialized = true;
+        draft.layoutCache = false;
+
+        /** 如果接口有 schema，就更新 schemaData */
+        if (schema) {
+          draft.schemaData = schema as any;
+        }
+        /** 只有当 config 中没有 nodes 和 edges 的时候，才会用 schema 生成一个默认样式 */
+
+        if (schema && (draft.config.nodes?.length === 0 || draft.config.edges?.length === 0)) {
+          const schemaStyle = utils.generatorStyleConfigBySchema(schema);
+          draft.config.nodes = schemaStyle.nodes;
+          draft.config.edges = schemaStyle.edges;
+        }
+        /** 如果有布局信息 */
         if (position) {
           draft.layout.type = 'preset';
         }
+        /** 如果有样式数据 */
         if (style) {
           draft.data = data;
           draft.source = data;
-        } else {
-          if (nodes.length > largeGraphLimit) {
-            console.warn(
-              '加载的数据量过大，建议聚合数据，默认切换到网格布局。您也可以在「资产中心」中加载「大图组件」启用 3D 渲染',
-            );
-            const newData = transform(data, true);
-            draft.largeGraphMode = true;
-            draft.largeGraphData = newData;
-            draft.source = newData;
-            draft.data = {
-              nodes: [],
-              edges: [],
-            };
-          } else {
-            const newData = transform(data, true);
-            draft.rawData = { ...data };
-            draft.data = newData;
-            draft.source = newData;
-            draft.largeGraphMode = false;
-            draft.largeGraphData = undefined;
-          }
+          return;
         }
-        draft.initialized = true;
-        draft.layoutCache = false;
+        /** 如果是大图模式 */
+        if (nodes.length > largeGraphLimit) {
+          console.warn(
+            '加载的数据量过大，建议聚合数据，默认切换到网格布局。您也可以在「资产中心」中加载「大图组件」启用 3D 渲染',
+          );
+          const newData = transform(data, true);
+          draft.largeGraphMode = true;
+          draft.largeGraphData = newData;
+          draft.source = newData;
+          draft.data = {
+            nodes: [],
+            edges: [],
+          };
+          return;
+        }
+        /** 如果是聚合模式 */
+        if (aggregate) {
+          const newData = transform(data, true);
+          draft.rawData = { ...data };
+          draft.source = newData;
+          draft.largeGraphMode = false;
+          draft.largeGraphData = undefined;
+          draft.data = transform(utils.aggregateEdges(data), true);
+          return;
+        }
+        /** 默认是普通模式 */
+        const newData = transform(data, true);
+        draft.rawData = { ...data };
+        draft.data = newData;
+        draft.source = newData;
+        draft.largeGraphMode = false;
+        draft.largeGraphData = undefined;
       });
     });
-  }, [largeGraphLimit]);
+  }, [largeGraphLimit, aggregate]);
 
   return null;
 };
