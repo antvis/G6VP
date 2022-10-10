33a3f08fec3f7d061e94dffc1b725aecce6e95ad
baizn <576375879@qq.com>
2022-10-10 10:54:10 +0800

    fix: 添加下拉导航容器组件

---
+++ packages/gi-assets-basic/src/components/SideSelectTabs/info.ts
@@ -0,0 +1,12 @@
+/**   index.md 中解析得到默认值，也可用户手动修改 */
+const info = {
+  id: 'SideSelectTabs',
+  name: '侧边下拉导航栏',
+  desc: '画布侧边下拉导航栏，可集成分析组件',
+  icon: 'icon-sidebar',
+  cover: 'http://xxxx.jpg',
+  category: 'container-components',
+  type: 'GICC',
+};
+
+export default info;
