627b74e6ed277082cb9e5dbea120f2efa92803fe
Moyee <576375879@qq.com>
2022-10-12 17:31:29 +0800

    fix: 修复GS单机版问题 (#27)

--- packages/gi-assets-gs-standalone/src/services/index.tsx
+++ packages/gi-assets-gs-standalone/src/services/index.tsx
@@ -6,9 +6,9 @@ import * as NeighborsQuery from './NeighborsQuery';
 import ServerComponent from './ServerComponent';
 
 export default {
-  id: 'GS',
-  name: 'GraphScope',
-  desc: 'GraphScope 引擎服务',
+  id: 'GS_Standalone',
+  name: 'GraphScopeStandalone',
+  desc: 'GraphScope 单机版引擎服务',
   type: 'database',
   component: ServerComponent,
   services: {
