e9535b169b46e8f31ab35ea086f19af51df2cfdf
Moyee <576375879@qq.com>
2022-10-13 18:31:54 +0800

    feat：Gremlin 发布模板及语句模板查询 (#28)
    * fix: Gremlin查询支持发布成模板功能
    
    * fix: Gremlin路径查询名称修改
    
    * fix: GI年内置发布模板服务
    
    * fix: 发布模板时验证模板名称
    
    * fix: 添加语句模板查询资产
    
    * feat: 追加services
    
    Co-authored-by: pomelo-nwu <czynwu@outlook.com>

---
+++ packages/gi-assets-advance/src/components/TemplateQuery/info.ts
@@ -0,0 +1,12 @@
+const info = {
+  id: 'TemplateQuery',
+  name: '语句模版查询',
+  desc: '通过模版查询图数据',
+  icon: 'icon-query',
+  category: 'data-query',
+  cover: 'http://xxxx.jpg',
+  type: 'GIAC_CONTENT',
+  services: ['TemplateListService', 'ExecTemplateQueryService'],
+};
+
+export default info;
---
+++ packages/gi-assets-advance/src/services/TemplateService.ts
@@ -0,0 +1,100 @@
+export interface ServiceObject {
+  name: string;
+  service: (params: any) => Promise<{}>;
+}
+import { utils } from '@alipay/graphinsight';
+
+const DEFAULT_TEMPLATE = {
+  queryTemplate: 'g.V({{ID}})',
+  templateId: '3283',
+  graphLanguageType: 'GREMLIN',
+  templateName: 'defaultTemplateName',
+  templateParameterList: [{ parameterName: 'ID', parameterValue: '1', valueType: 'STRING' }],
+};
+export const PublishTemplate: ServiceObject = {
+  name: '发布模板',
+  service: async params => {
+    const uid = 'mock_publish_template';
+    console.log('参数', params);
+
+    const { GI_SITE_PROJECT_ID: projectId } = utils.getProjectContext();
+    //@ts-ignore
+    const { localforage } = window;
+    const project = await localforage.getItem(projectId);
+    const gremlin_template = project._gremlin_template || [DEFAULT_TEMPLATE];
+    //project.themes = [...themes, theme];
+    const new_gremlin_template = [...gremlin_template, params];
+
+    localforage.setItem(projectId, { ...project, _gremlin_template: new_gremlin_template });
+
+    return new Promise(resolve => {
+      return resolve({
+        success: true,
+        message: 'GI 默认提供的发布模板的服务',
+        data: params,
+      });
+    });
+  },
+};
+
+export const TemplateListService = {
+  name: '查询模板列表',
+  service: async () => {
+    const { GI_SITE_PROJECT_ID: projectId } = utils.getProjectContext();
+    //@ts-ignore
+    const { localforage } = window;
+    const project = await localforage.getItem(projectId);
+    const gremlin_template = project._gremlin_template || [DEFAULT_TEMPLATE];
+
+    return new Promise(resolve => {
+      return resolve({
+        success: true,
+        message: 'GI 默认提供的发布模板的服务',
+        data: gremlin_template,
+      });
+    });
+  },
+};
+
+export const ExecTemplateQueryService = {
+  name: '执行模板查询',
+  service: params => {
+    console.log('执行模板查询参数', params);
+    const id = 'mock_template_query';
+    const data = {
+      nodes: [
+        {
+          id,
+          type: 'user',
+        },
+
+        {
+          id: `${id}-family`,
+          type: 'user',
+        },
+        {
+          id: `${id}-card`,
+          type: 'card',
+        },
+      ],
+      edges: [
+        {
+          source: id,
+          target: `${id}-family`,
+          type: 'family',
+        },
+        {
+          source: id,
+          target: `${id}-card`,
+          type: 'own',
+        },
+      ],
+    };
+    return new Promise(resolve => {
+      return resolve({
+        success: true,
+        data,
+      });
+    });
+  },
+};
