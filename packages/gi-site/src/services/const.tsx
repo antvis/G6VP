39154e0d02f8f0b26986aa04de6099f38b6b37a6
pomelo-nwu <czynwu@outlook.com>
2023-02-08 22:47:37 +0800

    chore: style code

--- packages/gi-site/src/services/const.tsx
+++ packages/gi-site/src/services/const.tsx
@@ -26,16 +26,7 @@ export const GI_SITE = {
 //@ts-ignore
 export const IS_DEV_ENV = process.env.NODE_ENV === 'development';
 
-export const ASSET_TYPE = {
-  COMPONENT: 1, // 1 表示组件
-  LAYOUT: 2, // 2 表示布局
-  SERVICE: 3, // 3 表示数据服务
-  NODE: 4, // 4 表示节点
-  EDGE: 5, // 5 表示边
-  PROJECT: 6, // 6 大分类 项目
-  ASSET: 7, // 7 大分类 分析资产
-};
-
+ 
 export const GI_LOGO_URL = {
   light: 'https://mdn.alipayobjects.com/huamei_qa8qxu/afts/img/A*DsXhSJ1x8DUAAAAAAAAAAAAADmJ7AQ/original',
   dark: 'https://mdn.alipayobjects.com/huamei_qa8qxu/afts/img/A*TawEQ6nGf-AAAAAAAAAAAAAADmJ7AQ/original',
