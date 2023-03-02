9e89789f92393c8ce51db3dd56ca8f721a2f024e
kongtan <jianping.kjp@antgroup.com>
2023-03-02 11:37:22 +0800

    节点描述完善

--- packages/gi-assets-basic/src/components/RemoveNodeWithMenu/info.ts
+++ packages/gi-assets-basic/src/components/RemoveNodeWithMenu/info.ts
@@ -1,7 +1,7 @@
 const info = {
   id: 'RemoveNodeWithMenu',
   name: '节点删除',
-  desc: '节点删除',
+  desc: '节点删除-右键菜单集成',
   icon: 'icon-minus-circle',
   cover: 'http://xxxx.jpg',
   category: 'elements-interaction',
