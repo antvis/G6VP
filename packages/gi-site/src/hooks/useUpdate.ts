c94970c2854bc867f30ba082fe57eef0d86d8bad
pomelo-nwu <czynwu@outlook.com>
2023-03-07 17:52:28 +0800

    feat: 处理异常报错

--- packages/gi-site/src/hooks/useUpdate.ts
+++ packages/gi-site/src/hooks/useUpdate.ts
@@ -20,37 +20,39 @@ window.GI_DATASET_DB = GI_DATASET_DB;
 window.GI_TEMPLATE_DB = GI_DATASET_DB;
 
 const useUpdate = async () => {
-  //@ts-ignore
-  console.time('update db');
-  const databases = await indexedDB.databases();
-  const isLatest =
-    databases.filter(item => {
-      return item.name == 'gi-project' || item.name === 'gi-dataset';
-    }).length === 2;
+  try {
+    const databases = await indexedDB.databases();
+    const isLatest =
+      databases.filter(item => {
+        return item.name == 'gi-project' || item.name === 'gi-dataset';
+      }).length === 2;
 
-  if (!isLatest) {
-    console.log('旧版本的数据库，需要升级');
-    localforage.iterate((item: IProject) => {
-      //@ts-ignore
-      const { id, engineId, engineContext, data, schemaData, ...others } = item;
-      const datasetId = `ds_${getUid()}`;
-      if (id) {
-        GI_PROJECT_DB.setItem(id, { ...others, id, datasetId });
-      }
-      GI_DATASET_DB.setItem(datasetId, {
-        id: datasetId,
-        type: 'user',
-        engineId,
-        engineContext,
-        data,
-        schemaData,
+    if (!isLatest) {
+      console.log('旧版本的数据库，需要升级');
+
+      localforage.iterate((item: IProject) => {
+        //@ts-ignore
+        const { id, engineId, engineContext, data, schemaData, ...others } = item;
+        const datasetId = `ds_${getUid()}`;
+        if (id) {
+          GI_PROJECT_DB.setItem(id, { ...others, id, datasetId });
+        }
+        GI_DATASET_DB.setItem(datasetId, {
+          id: datasetId,
+          type: 'user',
+          engineId,
+          engineContext,
+          data,
+          schemaData,
+        });
       });
-    });
 
-    await GI_PROJECT_DB.ready();
-    await GI_DATASET_DB.ready();
-    await localforage.dropInstance({ name: 'localforage' });
-    console.timeEnd('update db');
+      await GI_PROJECT_DB.ready();
+      await GI_DATASET_DB.ready();
+      await localforage.dropInstance({ name: 'localforage' });
+    }
+  } catch (error) {
+    console.error(error);
   }
 };
 export default useUpdate;
