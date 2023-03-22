fc162b4cc9c54940333a872d365b2a18a4fb5e85
pomelo.lcw <pomelo.lcw@antgroup.com>
2023-03-22 19:16:10 +0800

    feat: 支持查询limit配置

--- packages/gi-assets-tugraph/src/services/NeighborsQuery.ts
+++ packages/gi-assets-tugraph/src/services/NeighborsQuery.ts
@@ -5,7 +5,7 @@ export const NeighborsQuery = {
   name: '邻居查询',
   service: async params => {
     const { TUGRAPH_USER_TOKEN, HTTP_SERVICE_URL, CURRENT_TUGRAPH_SUBGRAPH } = utils.getServerEngineContext();
-    const { ids, sep } = params;
+    const { ids, sep, limit } = params;
     const response = await request(`${HTTP_SERVICE_URL}/api/tugraph/neighbors`, {
       method: 'post',
       headers: {
@@ -16,6 +16,7 @@ export const NeighborsQuery = {
         ids,
         sep,
         graphName: CURRENT_TUGRAPH_SUBGRAPH,
+        limit,
       },
     });
 
--- packages/gi-httpservice/app/service/tugraph.ts
+++ packages/gi-httpservice/app/service/tugraph.ts
@@ -176,12 +176,12 @@ class TuGraphService extends Service {
    * @param params
    */
   async queryNeighbors(params: INeighborsParams) {
-    const { ids, graphName = '', sep = 1, authorization = '' } = params;
-    let cypher = `match(n)-[*..${sep}]-(m) WHERE id(n)=${ids[0]} RETURN n, m LIMIT 100`;
+    const { ids, graphName = '', sep = 1, authorization = '', limit = 100 } = params;
+    let cypher = `match(n)-[*..${sep}]-(m) WHERE id(n)=${ids[0]} RETURN n, m LIMIT ${limit}`;
 
     if (ids.length > 1) {
       // 查询两度关系，需要先查询节点，再查询子图
-      cypher = `match(n)-[*..${sep}]-(m) WHERE id(n) in [${ids}] RETURN n, m LIMIT 200`;
+      cypher = `match(n)-[*..${sep}]-(m) WHERE id(n) in [${ids}] RETURN n, m LIMIT ${limit}`;
     }
 
     const responseData = await this.querySubGraphByCypher(cypher, graphName, authorization);
