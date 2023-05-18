b4e899d1cf2e6cb35e2b75625cde40b9b2118a51
Yanyan-Wang <yanyanwang93@gmail.com>
2023-05-18 22:08:35 +0800

    feat: 力导支持权重高级配置

--- packages/gi-sdk/src/process/getDefSideCoeFunction.ts
+++ packages/gi-sdk/src/process/getDefSideCoeFunction.ts
@@ -1,4 +1,4 @@
-export const getDefSideCoeFunction = (income: string, outcome: string, isLog: string, multiple: string) => {
+export const getDefSideCoeFunction = (income: string, outcome: string, isLog: boolean, multiple: string) => {
   const defSideCoe = (node): number => {
     let value = 0;
     try {
@@ -13,9 +13,39 @@ export const getDefSideCoeFunction = (income: string, outcome: string, isLog: st
       value = value * parseFloat(multiple);
       return value;
     } catch (e) {
-        console.log(e)
-    } 
+      console.log(e);
+    }
     return value;
   };
   return defSideCoe;
+};
+
+export const getDefSideCoeFromEdgeFunction = (amountFromEdge: string[], isLog: boolean, multiple: string) => {
+  const fieldMap = {};
+  amountFromEdge.forEach(weightField => {
+    const [edgeType, field] = weightField.split('^^');
+    fieldMap[edgeType] = field;
+  });
+  return (node, edges): number => {
+    if (!node || !edges?.length) return 0;
+    let outTotal = 0;
+    let inTotal = 0;
+    edges.forEach(edge => {
+      if (edge.source === node.id) {
+        const fieldName = fieldMap[edge.edgeType];
+        outTotal += Number(edge.data[fieldName]) || 0;
+      }
+      if (edge.target === node.id) {
+        const fieldName = fieldMap[edge.edgeType];
+        inTotal += Number(edge.data[fieldName]) || 0;
+      }
+    });
+    let val = inTotal - outTotal;
+    if (!val) return 0;
+    if (isLog) {
+      const sign = val >= 0 ? 1 : -1;
+      val = sign * Math.log(Math.abs(val));
+    }
+    return val * parseFloat(multiple);
+  };
 };
