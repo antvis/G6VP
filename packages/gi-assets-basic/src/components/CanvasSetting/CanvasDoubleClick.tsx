eb86ca6938e9f59406d94726754db74ca4c36d18
pomelo-nwu <czynwu@outlook.com>
2023-05-23 00:06:40 +0800

    feat: add features for assets

--- packages/gi-assets-basic/src/components/CanvasSetting/CanvasDoubleClick.tsx
+++ packages/gi-assets-basic/src/components/CanvasSetting/CanvasDoubleClick.tsx
@@ -10,7 +10,7 @@ const CanvasClick = () => {
 
   React.useEffect(() => {
     const handleCenter = () => {
-      graph.fitCenter(); // graph.fitCenter(true); 动画有bug
+      graph.fitCenter(true); // graph.fitCenter(true); 动画有bug
     };
     graph.on('canvas:dblclick', handleCenter);
     return () => {
