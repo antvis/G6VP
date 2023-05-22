992992ba927e1ca15a85d860c1914cfec9b83ccb
pomelo-nwu <czynwu@outlook.com>
2023-05-22 21:59:19 +0800

    fix: 解决build:all:es 报错问题

--- packages/gi-assets-tugraph/src/services/Server.tsx
+++ packages/gi-assets-tugraph/src/services/Server.tsx
@@ -1,11 +1,7 @@
 import { EngineServer } from '@antv/gi-sdk';
 import * as React from 'react';
-import {
-  connectTuGraphDataSource,
-  queryGraphSchema,
-  querySubGraphList,
-  queryVertexLabelCount,
-} from '../services/TuGraphService';
+import { connectTuGraphDataSource, querySubGraphList, queryVertexLabelCount } from '../services/TuGraphService';
+import { GI_SERVICE_INTIAL_GRAPH } from './Initializer';
 
 export interface ServerProps {}
 
@@ -18,7 +14,7 @@ const Server: React.FunctionComponent<ServerProps> = props => {
       desc="TuGraph 是蚂蚁集团自主研发的大规模图计算系统，提供图数据库引擎和图分析引擎。其主要特点是大数据量存储和计算，高吞吐率，以及灵活的 API，同时支持高效的在线事务处理（OLTP）和在线分析处理（OLAP）"
       logo="https://gw.alipayobjects.com/mdn/rms_3ff49c/afts/img/A*xqsZTKLVHPsAAAAAAAAAAAAAARQnAQ"
       //@ts-ignore
-      queryGraphSchema={queryGraphSchema}
+      queryGraphSchema={GI_SERVICE_INTIAL_GRAPH.service}
       //@ts-ignore
       querySubGraphList={querySubGraphList}
       queryVertexLabelCount={queryVertexLabelCount}
