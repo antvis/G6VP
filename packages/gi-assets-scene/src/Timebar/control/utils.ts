0ee94f7c3474e4f6b47860fca0763d5757148d22
Chushen <yoonagoddess@163.com>
2023-06-05 14:02:55 +0800

    fix: 修复timebar配置不生效问题

--- packages/gi-assets-scene/src/Timebar/control/utils.ts
+++ packages/gi-assets-scene/src/Timebar/control/utils.ts
@@ -1,6 +1,6 @@
-import dayjs from 'dayjs';
 import type { GIGraphData } from '@antv/gi-sdk';
-import type { Selection, TimeGranularity } from '../types';
+import dayjs from 'dayjs';
+import type { FieldType, Selection, TimeGranularity } from '../types';
 import { getTimeFormat } from './panel/helper';
 
 export function timeParser(time: number): number;
@@ -17,7 +17,7 @@ export function timeParser(time: number | string, timeGranularity?: TimeGranular
  * 根据时间范围筛选图数据
  * @param data 图数据
  * @param range 时间范围
- * @param timeFieldEdge 时间字段(边)
+ * @param timeField 时间字段(边)
  * @param timeFieldNode 时间字段(节点，默认与边相同)
  * @returns
  */
@@ -25,60 +25,63 @@ export function dataFilter(
   data: GIGraphData,
   range: Selection,
   timeGranularity: TimeGranularity,
-  timeFieldEdge: string,
-  timeFieldNode: string = timeFieldEdge,
-): GIGraphData {
-  const { nodes = [], edges = [] } = data;
+  timeField: string,
+  type: FieldType,
+) {
+  const parser = time => timeParser(time, timeGranularity);
 
-  const parser = t => timeParser(t, timeGranularity);
-
-  const edgesFiltered = edges.filter(edge => {
-    const time = parser(edge.data[timeFieldEdge]);
+  const baseFiltrerd = (data[type] as any[]).filter(item => {
+    const time = parser(item.data[timeField]);
     return time >= parser(range[0]) && time <= parser(range[1]);
   });
 
-  let nodesFiltered: GIGraphData['nodes'] = [];
-  if (nodes.length > 0) {
-    if (nodes[0].data[timeFieldNode]) {
-      nodesFiltered = nodes.filter(node => {
-        const time = parser(node.data[timeFieldNode]);
+  let anotherFiltered: any[] = [];
+  const another: any[] = data[type === 'nodes' ? 'edges' : 'nodes'];
+  if (another.length > 0) {
+    if (another[0].data[timeField]) {
+      anotherFiltered = another.filter(item => {
+        const time = parser(item.data[timeField]);
         return time >= parser(range[0]) && time <= parser(range[1]);
       });
     }
-    // 如果节点数据中没有时间字段，根据边数据进行筛选
+    // 如果节点数据中没有时间字段，根据 base 数据进行筛选
     else {
-      const allNodesFromEdges = edgesFiltered.reduce((acc, cur) => {
-        acc.add(cur.source);
-        acc.add(cur.target);
-        return acc;
-      }, new Set<string>([]));
-      nodesFiltered = nodes.filter(node => allNodesFromEdges.has(node.id));
+      if (type === 'edges') {
+        const allNodesFromEdges = baseFiltrerd.reduce((acc, cur) => {
+          acc.add(cur.source);
+          acc.add(cur.target);
+          return acc;
+        }, new Set<string>([]));
+        anotherFiltered = another.filter(node => allNodesFromEdges.has(node.id));
+      } else {
+        const allEdgesFromNodes = baseFiltrerd.reduce((acc, cur) => {
+          acc.add(cur.id);
+          return acc;
+        }, new Set<string>([]));
+        anotherFiltered = another.filter(
+          edge => allEdgesFromNodes.has(edge.source) && allEdgesFromNodes.has(edge.target),
+        );
+      }
     }
   }
 
   return {
-    nodes: nodesFiltered,
-    edges: edgesFiltered,
-  };
+    [type]: baseFiltrerd,
+    [type === 'nodes' ? 'edges' : 'nodes']: anotherFiltered,
+  } as unknown as GIGraphData;
 }
 
 /**
  * 获取图数据中的时间范围
  */
-export function getTimeRange(
-  data: GIGraphData,
-  timeGranularity: TimeGranularity,
-  timeFieldEdge: string,
-  timeFieldNode: string = timeFieldEdge,
-) {
-  const { nodes, edges } = data;
+export function getTimeRange(data: Record<string, any>[], timeGranularity: TimeGranularity, timeField: string) {
   const parser = t => timeParser(t, timeGranularity);
-  const nodesTime = nodes
-    .map(node => node.data[timeFieldNode])
+
+  const times = data
+    .map(datum => datum[timeField])
     .filter(Boolean)
     .map(parser);
-  const edgesTime = edges.map(edge => edge.data[timeFieldEdge]).map(parser);
-  const times = [...nodesTime, ...edgesTime];
+
   const timeRange: Selection = [Math.min(...times), Math.max(...times)];
   return timeRange;
 }
@@ -87,6 +90,6 @@ export function getTimeRange(
  * 转换图数据为 G2 渲染数据
  * @param data 图数据
  */
-export function dataTransform(data: GIGraphData, type: 'nodes' | 'edges') {
+export function dataTransform(data: GIGraphData, type: FieldType) {
   return data[type].map(node => node.data);
 }
