db30b3e755d29d871b8aacc291541dca136a6ae2
Chushen <yoonagoddess@163.com>
2023-06-08 18:42:48 +0800

    fix: 修复keys重复的问题

--- packages/gi-common-components/src/Utils/getAllkeysBySchema.ts
+++ packages/gi-common-components/src/Utils/getAllkeysBySchema.ts
@@ -1,14 +1,14 @@
 export const getAllkeysBySchema = (schema, shapeType: 'nodes' | 'edges') => {
   try {
-    const keySet = new Set<[string, string]>();
+    const keyMap = new Map<string, string>();
     // 默认要把 nodeType/edgeType 加进来
-    keySet.add([shapeType === 'nodes' ? 'nodeType' : 'edgeType', 'string']);
+    keyMap.set(shapeType === 'nodes' ? 'nodeType' : 'edgeType', 'string');
     schema[shapeType].forEach(item => {
       Object.entries(item.properties as Record<string, string>).forEach(k => {
-        keySet.add(k);
+        keyMap.set(...k);
       });
     });
-    return [...keySet];
+    return Array.from(keyMap);
   } catch (error) {
     return [];
   }
