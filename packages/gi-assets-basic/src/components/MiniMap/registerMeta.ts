fa09326732cf80105799f3be7039eca0ebc16af2
yvonneyx <zhuyuxin@gmail.com>
2023-08-03 10:40:55 +0800

    perf: minimap支持配置定位

--- packages/gi-assets-basic/src/components/MiniMap/registerMeta.ts
+++ packages/gi-assets-basic/src/components/MiniMap/registerMeta.ts
@@ -1,3 +1,29 @@
-export default () => {
-  return {};
+import { PLACEMENT_OPTIONS } from '../const';
+
+const registerMeta = () => {
+  const schema = {
+    placement: {
+      title: '组件位置',
+      type: 'string',
+      'x-decorator': 'FormItem',
+      'x-component': 'Select',
+      enum: PLACEMENT_OPTIONS,
+      default: 'RB',
+    },
+    offset: {
+      title: '偏移量',
+      type: 'string',
+      'x-decorator': 'FormItem',
+      'x-component': 'Offset',
+      'x-component-props': {
+        min: 0,
+        max: 400,
+      },
+      default: [0, 0],
+    },
+  };
+
+  return schema;
 };
+
+export default registerMeta;
