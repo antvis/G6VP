97f37fec14bbe748ba3b5a8bcf152dbe9fa0672a
pomelo-nwu <czynwu@outlook.com>
2023-08-04 18:09:33 +0800

    feat: 更新 container 的机制

--- packages/gi-sdk/src/hooks/useContainer.tsx
+++ packages/gi-sdk/src/hooks/useContainer.tsx
@@ -1,6 +1,32 @@
 import { Empty } from 'antd';
 import React from 'react';
 
+const compatibleContainers = containers => {
+  /**
+   * hack start
+   *
+   * 不应该修改 registerMeta 原有的containers 数据结构
+   * 1. 先把追加的 GI_FreeContainer 移除
+   * 2. 把 GI_CONTAINER 中的 数组对象 改为字符串,有的时候，修改容器内容，又会变成string
+   *
+   * TODO：
+   * 需要在gi-site层修改这个containers的值
+   *
+   */
+  return containers.slice(0, -1).map(item => {
+    return {
+      ...item,
+      GI_CONTAINER: item.GI_CONTAINER.map(item => {
+        if (item.value) {
+          return item.value;
+        }
+        return item;
+      }),
+    };
+  });
+  /** hack end */
+};
+
 /**
  * 获取根据容器资产配置，获取容器内资产实例
  *
@@ -8,9 +34,10 @@ import React from 'react';
  * @param containers 容器资产 props.containers
  * @returns
  */
-const useContainer = (context, containers) => {
+const useContainer = (context, _containers?: any) => {
   const { assets, config } = context;
-
+  const { pageLayout } = config;
+  const containers = compatibleContainers(pageLayout.props.containers);
   const ComponentCfgMap = config.components.reduce((acc, curr) => {
     return {
       ...acc,
@@ -19,12 +46,13 @@ const useContainer = (context, containers) => {
   }, {});
   const getComponentById = componentId => {
     const asset = assets.components[componentId];
-    if (!asset) {
+    const assetConfig = ComponentCfgMap[componentId];
+    if (!asset || !assetConfig) {
       console.warn(`asset: ${componentId} not found`);
       return null;
     }
     const { component: Component } = asset;
-    const { props: componentProps } = ComponentCfgMap[componentId];
+    const { props: componentProps } = assetConfig;
     const { icon } = componentProps.GIAC_CONTENT || {};
     return {
       id: componentId,
