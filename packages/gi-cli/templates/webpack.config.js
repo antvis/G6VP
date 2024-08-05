a9a2f14159def0dad69e102b6dbc5c9da4902211
mutu <gegenhasi.gghs@antgroup.com>
2024-08-05 20:59:19 +0800

    chore: fix build error

--- packages/gi-cli/templates/webpack.config.js
+++ packages/gi-cli/templates/webpack.config.js
@@ -39,8 +39,8 @@ module.exports = (env, argv) => {
             presets: ['@babel/env', '@babel/preset-react'],
             plugins: [
               ['@babel/plugin-transform-class-properties', { loose: true }],
-              ['@babel/plugin-proposal-private-methods', { loose: true }],
-              ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+              ['@babel/plugin-transform-private-methods', { loose: true }],
+              ['@babel/plugin-transform-private-property-in-object', { loose: true }],
               ['react-hot-loader/babel'],
             ],
           },
